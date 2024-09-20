26a24d9e82a2a214da8a5e406fc0d713779d61b8
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-09-20 12:33:29 +0900

    feat: Text Component 생성 (#5)
    * chore: css 초기화 코드 추가
    
    * feat: Text 컴포넌트 생성
    
    * design: tailwind 적용
    
    * refactor: 코드 리뷰 반영

---
+++ components/ui/Text.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+interface TextProps {
+  children: React.ReactNode;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  weight?: 'light' | 'regular' | 'bold';
+  underline?: boolean;
+  block?: boolean;
+  transform?: 'capitalize' | 'uppercase' | 'lowercase';
+  align?: 'left' | 'center' | 'right';
+  lineClamp?: number;
+  color?: 'primary' | 'black' | 'gray' | 'white';
+  inherit?: boolean;
+}
+
+function Text({
+  children,
+  size = 'md',
+  weight = 'regular',
+  underline = false,
+  block = false,
+  transform,
+  align,
+  lineClamp,
+  color = 'black',
+  inherit = false,
+}: TextProps): JSX.Element {
+  // 말줄임 설정 위한 코드
+  let displayStyle: string = 'inline';
+  if (lineClamp) {
+    displayStyle = '-webkit-box';
+  } else if (block) {
+    displayStyle = 'block';
+  }
+
+  const colorClass =
+    {
+      primary: 'text-primary', // #007BFF
+      black: 'text-black',
+      gray: 'text-gray-500',
+      white: 'text-white',
+    }[color] || 'text-black'; // 기본값
+
+  const sizeClass =
+    {
+      xs: 'text-xs',
+      sm: 'text-sm',
+      md: 'text-base',
+      lg: 'text-xl',
+      xl: 'text-2xl',
+    }[size] || 'text-base';
+
+  const weightClass =
+    {
+      light: 'font-extralight',
+      regular: 'font-medium',
+      bold: 'font-extrabold',
+    }[weight] || 'regular';
+
+  const baseStyles: React.CSSProperties = {
+    display: displayStyle,
+    textDecoration: underline ? 'underline' : 'none',
+    textTransform: transform,
+    textAlign: align,
+    color: inherit ? 'inherit' : color || 'black',
+    WebkitLineClamp: lineClamp,
+    overflow: lineClamp ? 'hidden' : 'visible',
+    WebkitBoxOrient: lineClamp ? 'vertical' : undefined,
+  };
+
+  return (
+    <span
+      style={{ ...baseStyles }}
+      className={[colorClass, sizeClass, weightClass].join(' ')}
+    >
+      {children}
+    </span>
+  );
+}
+
+export default Text;
