ec8d87169b05bbe83a977685c18f795a3d3e7381
iamgyu <iamgyu@naver.com>
2024-09-20 12:46:59 +0900

    feat: Input Component 생성 (#7)
    * feat: storybook과 shadcn을 이용하여 input 컴포넌트 생성
    
    * chore: git 대소문자 문제 해결
    
    * chore: git 대소문자 문제 해결
    
    * chore: size, radius 단위 수정
    
    * feat: search input 구현
    
    * refactor: input 컴포넌트 코드 수정
    
    * refactor: input padding 및 이미지 색상 코드 수정

---
+++ components/ui/Input.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './Input';
+
+const meta: Meta<typeof Input> = {
+  title: 'Components/ui/Input',
+  component: Input,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    placeholder: {
+      control: { type: 'text' },
+    },
+    size: {
+      control: 'select',
+      dscription: 'Input sizes',
+      options: ['sm', 'md', 'lg'],
+    },
+    radius: {
+      control: 'select',
+      description: 'Input radius',
+      options: ['sm', 'md', 'lg', 'round'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+function UserIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="gray"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-search"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </svg>
+  );
+}
+
+export const Default: Story = {
+  args: {
+    size: 'md',
+    radius: 'md',
+  },
+};
+
+export const Search: Story = {
+  args: {
+    size: 'md',
+    radius: 'md',
+    icon: <UserIcon />,
+  },
+};
