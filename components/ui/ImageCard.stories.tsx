2fe3e81f03adc6d7429a7351af7fc2624cf87179
Yejin0O0 <yyj11kr@naver.com>
2024-09-23 14:33:07 +0900

    chore: Story 타입 변경

--- components/ui/ImageCard.stories.tsx
+++ components/ui/ImageCard.stories.tsx
@@ -19,7 +19,7 @@ const meta: Meta<typeof ImageCard> = {
 
 export default meta;
 
-type Story = StoryObj<typeof ImageCard>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
