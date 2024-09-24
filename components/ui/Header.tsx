bdef7df202332987abcb1cd7c2c59421f38adf94
Yejin0O0 <yyj11kr@naver.com>
2024-09-24 12:50:13 +0900

    feat: Header 컴포넌트 생성

---
+++ components/ui/Header.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { UserRound } from 'lucide-react';
+import { UserStateProps } from '@/types/layoutTypes';
+import { Input } from './Input';
+import { LinkText } from './Text';
+
+const personalSection = (isLogin: boolean, isJoined: boolean) => {
+  if (!isLogin) {
+    return (
+      <div className="flex w-44 justify-evenly">
+        <LinkText
+          color="gray"
+          className="text-sm cursor-pointer text-center leading-6 font-normal"
+          link="/"
+        >
+          동호회 새로 만들기
+        </LinkText>
+        <LinkText
+          color="primary"
+          className="text-sm cursor-pointer leading-6"
+          link="/"
+        >
+          Login
+        </LinkText>
+      </div>
+    );
+  }
+  if (!isJoined) {
+    return (
+      <div className="flex w-40 justify-evenly">
+        <LinkText
+          color="gray"
+          className="text-sm cursor-pointer text-center leading-6 font-normal"
+          link="/"
+        >
+          동호회 새로 만들기
+        </LinkText>
+        <UserRound
+          size={24}
+          className="text-black cursor-pointer group-hover:text-white transition-colors"
+        />
+      </div>
+    );
+  }
+  return (
+    // return <LucideIcon color={color} size={size} />;
+    <UserRound size={24} cursor-pointer color="#000000" />
+  );
+};
+
+function Header(props: UserStateProps) {
+  const { isLogin, isJoined } = props;
+  return (
+    <div className="flex items-center justify-between space-x-4 w-full max-w-5xl h-16 px-8 border-solid border-b border-gray-200">
+      <div className="text-xl font-semibold cursor-pointer">LOGO</div>
+
+      <div className="flex items-center justify-end space-x-2 w-1/2">
+        <div className="w-1/2">
+          <Input search radius="round" placeholder="" size="sm" />
+        </div>
+        {personalSection(isLogin, isJoined)}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- components/ui/Input.stories.tsx
+++ components/ui/Input.stories.tsx
@@ -38,9 +38,9 @@ function UserIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="gray"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-badge-alert"
     >
       <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
