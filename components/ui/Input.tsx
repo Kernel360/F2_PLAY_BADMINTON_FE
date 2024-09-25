82f21315a1f8ca552538bf0eac59a6ee65e275ab
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-09-25 11:17:15 +0900

    feat: 헤더 컴포넌트 생성 (#20)
    * refactor: Input 컴포넌트 search, icon props 추가
    
    * feat: Header 컴포넌트 생성
    
    * chore: line 에러로 인한 수정
    
    * design: header css 수정
    
    * design: header padding 제거
    
    * feat: header 스토리북 생성
    
    * refactor: LinkText 속성 적용

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -60,14 +60,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           <span className={cn('inset-y-0 left-0 flex items-center')}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="20"
+              height="20"
               viewBox="0 0 24 24"
               fill="none"
               stroke="gray"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="lucide lucide-search"
             >
               <circle cx="11" cy="11" r="8" />
