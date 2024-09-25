2b8b4d38eb009e2c8516c1a86976b094d67a6a19
iamgyu <iamgyu@naver.com>
2024-09-25 12:15:43 +0900

    feat: IconButton 컴포넌트 생성 (#22)
    * feat: IconButton 컴포넌트 생성
    
    * fix: lint 에러 수정
    
    * refactor: 코멘트 내용 수정

---
+++ components/ui/IconButton.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Users } from 'lucide-react';
+import IconButton from './IconButton';
+
+const meta: Meta<typeof IconButton> = {
+  title: 'Components/ui/IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+
+  argTypes: {
+    size: {
+      control: 'inline-radio',
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+    },
+    radius: {
+      control: 'inline-radio',
+      options: ['sm', 'md', 'lg', 'round'],
+    },
+    color: {
+      control: 'inline-radio',
+      options: ['gray', 'primary', 'transparent'],
+    },
+    outline: {
+      control: 'boolean',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    size: 'md',
+    radius: 'md',
+    color: 'gray',
+    outline: false,
+  },
+
+  render: (args) => {
+    return (
+      <IconButton {...args}>
+        <Users width="80%" height="80%" color="black" />
+      </IconButton>
+    );
+  },
+};
