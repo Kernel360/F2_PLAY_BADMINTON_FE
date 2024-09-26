5cc0c14ff484abd9eb0bed546d49a0e30479d4cd
iamgyu <iamgyu@naver.com>
2024-09-26 12:47:22 +0900

    refactor: layout 삭제

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import LoginLayout from './layout';
+import Link from 'next/link';
+import Image from '@/components/ui/Image';
+import brand from '@/app/images/main-logo.png';
+import LoginButton from './LoginButton';
 
 function LoginPage() {
-  return <LoginLayout />;
+  return (
+    <div className="flex flex-col justify-center items-center p-16">
+      <Image src={brand.src} alt="brandLogo" width={288} height={288} />
+      <div className="flex flex-col mt-16 gap-8">
+        <Link href="http://3.38.247.217:8080/oauth2/authorization/naver`">
+          <LoginButton method="naver" />
+        </Link>
+        <Link href="http://3.38.247.217:8080/oauth2/authorization/google">
+          <LoginButton method="google" />
+        </Link>
+        <Link href="http://3.38.247.217:8080/oauth2/authorization/kakao">
+          <LoginButton method="kakao" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
