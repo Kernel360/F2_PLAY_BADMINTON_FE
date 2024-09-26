20a7a09c3cf1f5e5124f69cb5b9fa68764430ae6
iamgyu <iamgyu@naver.com>
2024-09-26 15:19:07 +0900

    feat: 로그인 페이지 생성 (#31)
    * feat: LoginButton 생성
    
    * fix: app router 사용 에러 수정
    
    * feat: Oauth 로그인 링크 버튼에 연결
    
    * fix: lint 에러 수정 및 코멘트 반영
    
    * refactor: layout 삭제
    
    * fix: 백틱 수정 및 로그인 Url 상수처리
    
    * fix: lint 에러 해결

---
+++ constants/loginOauthUrl.ts
@@ -0,0 +1,8 @@
+export const NAVER_OAUTH_URL =
+  'http://3.38.247.217:8080/oauth2/authorization/naver';
+
+export const GOOGLE_OAUTH_URL =
+  'http://3.38.247.217:8080/oauth2/authorization/google';
+
+export const KAKAO_OAUTH_URL =
+  'http://3.38.247.217:8080/oauth2/authorization/kakao';
