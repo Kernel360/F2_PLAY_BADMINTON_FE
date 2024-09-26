46a60c423e68ae987c3912340978bedb4576a6d3
Yejin0O0 <yyj11kr@naver.com>
2024-09-26 17:17:42 +0900

    fix: 레이아웃 변경
    - root layout 변경
    - 탭 클릭 시 url 변경
    - 탭 min-height 지정

---
+++ app/(header)/club/layout.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/Tabs';
+import Link from 'next/link';
+
+function ClubLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Tabs defaultValue="intro" className="w-full max-w-5xl mt-10 ">
+      <TabsList>
+        <Link href="/club/intro">
+          <TabsTrigger value="intro" color="gray">
+            소개
+          </TabsTrigger>
+        </Link>
+        <Link href="/club/schedule">
+          <TabsTrigger value="schedule" color="gray">
+            일정
+          </TabsTrigger>
+        </Link>
+        <Link href="/club/member">
+          <TabsTrigger value="member" color="gray">
+            회원
+          </TabsTrigger>
+        </Link>
+        <Link href="/club/manage">
+          <TabsTrigger value="manage" color="gray">
+            관리
+          </TabsTrigger>
+        </Link>
+      </TabsList>
+      <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow flex-1 space-y-4 p-8 min-h-[530px]">
+        {children}
+      </div>
+    </Tabs>
+  );
+}
+
+export default ClubLayout;
--- app/club/intro/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ClubIntroPages() {
-  return <div>intro</div>;
-}
-
-export default ClubIntroPages;
