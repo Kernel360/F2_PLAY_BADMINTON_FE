1348ebf3349c139cba13679dc39ef805ed7f8970
iamgyu <iamgyu@naver.com>
2024-09-26 15:19:07 +0900

    feat: 로그인 페이지 생성 (#31)
    * feat: LoginButton 생성
    
    * fix: app router 사용 에러 수정
    
    * feat: Oauth 로그인 링크 버튼에 연결
    
    * fix: lint 에러 수정 및 코멘트 반영
    
    * refactor: layout 삭제
    
    * fix: 백틱 수정 및 로그인 Url 상수처리
    
    * fix: lint 에러 해결

---
+++ app/login/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from '@/components/ui/Image';
+import brand from '@/app/images/main-logo.png';
+import {
+  GOOGLE_OAUTH_URL,
+  KAKAO_OAUTH_URL,
+  NAVER_OAUTH_URL,
+} from '@/constants/loginOauthUrl';
+import LoginButton from './LoginButton';
+
+function LoginPage() {
+  return (
+    <div className="flex flex-col justify-center items-center p-16">
+      <Image src={brand.src} alt="brandLogo" width={288} height={288} />
+      <div className="flex flex-col mt-16 gap-8">
+        <Link href={NAVER_OAUTH_URL}>
+          <LoginButton method="naver" />
+        </Link>
+        <Link href={GOOGLE_OAUTH_URL}>
+          <LoginButton method="google" />
+        </Link>
+        <Link href={KAKAO_OAUTH_URL}>
+          <LoginButton method="kakao" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
