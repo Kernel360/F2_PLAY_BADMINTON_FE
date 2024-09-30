cfa139073684d087a160e54521d2c2de1824b356
iamgyu <iamgyu@naver.com>
2024-09-30 15:11:07 +0900

    feat: mock data 추가

--- app/(header)/club/schedule/LeagueModal.tsx
+++ app/(header)/club/schedule/LeagueModal.tsx
@@ -10,42 +10,70 @@ import {
 import { Button } from '@/components/ui/Button';
 import { Textarea } from '@/components/ui/textarea';
 
+interface League {
+  leagueName: string;
+  description: string;
+  tierLimit: 'GOLD' | 'SILVER' | 'BRONZE';
+  status: 'OPEN' | 'CLOSED';
+  matchType: 'SINGLE' | 'DOUBLES';
+  leagueAt: string;
+  closedAt: string;
+  playerCount: number;
+  createdAt: string;
+  modifiedAt: string;
+  matchingRequirement: string;
+}
+
+const sampleLeague: League = {
+  leagueName: 'example',
+  description: 'description',
+  tierLimit: 'GOLD',
+  status: 'OPEN',
+  matchType: 'SINGLE',
+  leagueAt: '2024-09-30T05:53:45.509Z',
+  closedAt: '2024-09-30T05:53:45.509Z',
+  playerCount: 10,
+  createdAt: '2024-09-30T05:53:45.509Z',
+  modifiedAt: '2024-09-30T05:53:45.509Z',
+  matchingRequirement: 'TIER',
+};
+
 function LeagueModal() {
+  // TODO(iamgyu): 서버에서 데이터 받아오기
+  const league = sampleLeague;
+
   return (
     <Dialog>
       <DialogTrigger>Open</DialogTrigger>
       <DialogContent className="w-[100vw]">
-        <DialogHeader>
-          <DialogTitle>일정 제목</DialogTitle>
-        </DialogHeader>
         <div className="flex justify-center">
-          <h2>일정 제목</h2>
+          <h2>{league.leagueName}</h2>
           <div className="flex gap-4">
             <Button>b1</Button>
             <Button>b2</Button>
             <Button>b3</Button>
           </div>
         </div>
-        <Textarea>내용</Textarea>
+        <Textarea>{league.description}</Textarea>
         <div className="flex gap-4 w-full">
           <span className="flex justify-center w-1/2 px-4 border-2 border-gray-400 rounded-md">
-            00월 00일 00시
+            {league.leagueAt}
           </span>
           <span className="flex justify-center w-1/2 px-4 border-2 border-gray-400 rounded-md">
-            00체육관
+            장소
           </span>
         </div>
         <div className="flex justify-center w-full px-4 border-2 border-gray-400 rounded-md">
-          상/중/하/아무나
+          {league.tierLimit}
         </div>
         <div className="flex justify-center w-full px-4 border-2 border-gray-400 rounded-md">
-          00월 00일 00시 까지 모집
+          {league.closedAt}
         </div>
         <div className="flex justify-center w-full px-4 border-2 border-gray-400 rounded-md">
-          0명 / N명
+          {league.playerCount}
         </div>
         <div className="flex justify-center w-full px-4 border-2 border-gray-400 rounded-md">
-          단식/복식
+          {league.matchType}
         </div>
         <div className="flex justify-center w-full px-4 border-2 border-gray-400 rounded-md">
           대진표 생성
