e0fa214f517e2f5ffcab7f577eefc65a59a3f1e5
Yejin0O0 <yyj11kr@naver.com>
2024-10-02 12:43:18 +0900

    chore: fix lint error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import DayCell from '@/components/DayCell';
-import { Calendar } from '@/components/ui/calendar';
-import { ko } from 'date-fns/locale';
-import * as React from 'react';
+import DayCell from "@/components/DayCell";
+import { Calendar } from "@/components/ui/calendar";
+import { ko } from "date-fns/locale";
+import * as React from "react";
 
 export default function CalendarDemo() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
@@ -17,12 +17,12 @@ export default function CalendarDemo() {
         locale={ko}
         className="rounded-md text-gray-800"
         classNames={{
-          head_cell: 'w-[80px]',
-          cell: 'h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center hover:bg-primary/10',
-          day: 'hover:bg-inherit hover:text-inherit h-[80px] w-[80px] flex justify-center items-center',
+          head_cell: "w-[80px]",
+          cell: "h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center hover:bg-primary/10",
+          day: "hover:bg-inherit hover:text-inherit h-[80px] w-[80px] flex justify-center items-center",
           day_selected:
-            'w-full border border-primary bg-inherit text-primary hover:bg-inherit hover:text-inherit',
-          day_today: 'hover: bg-inherit',
+            "w-full border border-primary bg-inherit text-primary hover:bg-inherit hover:text-inherit",
+          day_today: "hover: bg-inherit",
         }}
         components={{
           DayContent: DayCell,
