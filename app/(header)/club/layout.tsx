b301fcaacfa713e11915f82e57f95a9d99352da0
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-02 11:48:13 +0900

    Update layout.tsx for lint

--- app/(header)/club/layout.tsx
+++ app/(header)/club/layout.tsx
@@ -6,7 +6,7 @@ import type React from "react";
 
 function ClubLayout({ children }: { children: React.ReactNode }) {
   const headerList = headers();
-  const pathname = headerList.get('x-current-path')?.split('/')[2];
+  const pathname = headerList.get("x-current-path")?.split("/")[2];
 
   return (
     <Tabs defaultValue={pathname} className="w-full max-w-5xl mt-10 ">
