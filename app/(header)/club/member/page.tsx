aa8e17267bfa95ecdbebd316356ebed1d6bf84bb
Yejin0O0 <yyj11kr@naver.com>
2024-10-02 12:15:51 +0900

    chore: 린트 에러 해결

--- app/(header)/club/member/page.tsx
+++ app/(header)/club/member/page.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import OneMemberInfo from './OneMemberInfo';
+import React, { useState } from "react";
+import OneMemberInfo from "./OneMemberInfo";
 
 function ClubMemberPage() {
   const [openDropdownIndex, setOpenDropdownIndex] = useState<number | null>(
