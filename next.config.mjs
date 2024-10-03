dd67df52df6e7f3705b4b9c868c6c158ce28d979
Yejin0O0 <yyj11kr@naver.com>
2024-10-03 16:35:35 +0900

    chore: storybook 배포 위한 next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// 스토리북 배포 디버깅 용
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.node = {
+        __dirname: true, // 서버에서만 __dirname 활성화
+      };
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
