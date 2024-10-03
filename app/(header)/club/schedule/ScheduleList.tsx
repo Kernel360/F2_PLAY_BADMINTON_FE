e09e3f1688e9e18bf97418dde54e4ef936dcd8ac
Yejin0O0 <yyj11kr@naver.com>
2024-10-03 19:49:40 +0900

    chore: biome 에러 해결

--- app/(header)/club/schedule/ScheduleList.tsx
+++ app/(header)/club/schedule/ScheduleList.tsx
@@ -1,90 +1,100 @@
-import { Text } from '@/components/ui/Text';
-import React from 'react';
+import { Text } from "@/components/ui/Text";
+import React from "react";
 
 function ScheduleList() {
   const schedules = [
     {
-      title: '일정 제목 1',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'gold',
-      type: '단식',
+      id: 1,
+      title: "일정 제목 1",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "gold",
+      type: "단식",
     },
     {
-      title: '일정 제목 2',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'silver',
-      type: '단식',
+      id: 2,
+      title: "일정 제목 2",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "silver",
+      type: "단식",
     },
     {
-      title: '일정 제목 3',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'bronze',
-      type: '단식',
+      id: 3,
+      title: "일정 제목 3",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "bronze",
+      type: "단식",
     },
     {
-      title: '일정 제목 4',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'gold',
-      type: '단식',
+      id: 4,
+      title: "일정 제목 4",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "gold",
+      type: "단식",
     },
     {
-      title: '일정 제목 5',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'silver',
-      type: '단식',
+      id: 5,
+      title: "일정 제목 5",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "silver",
+      type: "단식",
     },
     {
-      title: '일정 제목 6',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'bronze',
-      type: '단식',
+      id: 6,
+      title: "일정 제목 6",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "bronze",
+      type: "단식",
     },
     {
-      title: '일정 제목 7',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'gold',
-      type: '단식',
+      id: 7,
+      title: "일정 제목 7",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "gold",
+      type: "단식",
     },
     {
-      title: '일정 제목 8',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'silver',
-      type: '단식',
+      id: 8,
+      title: "일정 제목 8",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "silver",
+      type: "단식",
     },
     {
-      title: '일정 제목 9',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'bronze',
-      type: '단식',
+      id: 9,
+      title: "일정 제목 9",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "bronze",
+      type: "단식",
     },
     {
-      title: '일정 제목 10',
-      deadline: '8/12',
-      participants: '8/24 명',
-      tier: 'gold',
-      type: '단식',
+      id: 10,
+      title: "일정 제목 10",
+      deadline: "8/12",
+      participants: "8/24 명",
+      tier: "gold",
+      type: "단식",
     },
   ];
 
   const getTierWithEmoji = (tier: string) => {
     switch (tier) {
-      case 'gold':
-        return '🥇 골드';
-      case 'silver':
-        return '🥈 실버';
-      case 'bronze':
-        return '🥉 브론즈';
+      case "gold":
+        return "🥇 골드";
+      case "silver":
+        return "🥈 실버";
+      case "bronze":
+        return "🥉 브론즈";
       default:
-        return '';
+        return "";
     }
   };
 
@@ -97,9 +107,9 @@ function ScheduleList() {
       </div>
 
       <div className="grid gap-4 h-[27rem] overflow-y-auto">
-        {schedules.map((schedule, index) => (
+        {schedules.map((schedule) => (
           <div
-            key={index}
+            key={schedule.id}
             className="bg-white py-4 px-6 rounded-xl border border-solid hover:shadow-lg transform  transition-transform duration-300 cursor-pointer"
           >
             <div className="flex justify-between items-center mb-4">
--- app/(header)/club/schedule/page.tsx
+++ app/(header)/club/schedule/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import DayCell from '@/components/DayCell';
-import { Calendar } from '@/components/ui/calendar';
-import { ko } from 'date-fns/locale';
-import * as React from 'react';
-import ScheduleList from './ScheduleList';
+import DayCell from "@/components/DayCell";
+import { Calendar } from "@/components/ui/calendar";
+import { ko } from "date-fns/locale";
+import * as React from "react";
+import ScheduleList from "./ScheduleList";
 
 export default function ClubSchedulePage() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
@@ -18,12 +18,12 @@ export default function ClubSchedulePage() {
         locale={ko}
         className="rounded-md text-gray-800"
         classNames={{
-          head_cell: 'w-[80px]',
-          cell: 'h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center hover:bg-primary/10',
-          day: 'hover:bg-inherit hover:text-inherit h-[80px] w-[80px] flex justify-center items-center',
+          head_cell: "w-[80px]",
+          cell: "h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center hover:bg-primary/10",
+          day: "hover:bg-inherit hover:text-inherit h-[80px] w-[80px] flex justify-center items-center",
           day_selected:
-            'w-full border border-primary bg-inherit text-primary hover:bg-inherit hover:text-inherit',
-          day_today: 'hover: bg-inherit',
+            "w-full border border-primary bg-inherit text-primary hover:bg-inherit hover:text-inherit",
+          day_today: "hover: bg-inherit",
         }}
         components={{
           DayContent: DayCell,
