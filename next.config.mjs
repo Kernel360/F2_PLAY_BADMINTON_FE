9f6b04c9469d840b8e8d7242cd7f48b33d9c13f2
Yejin0O0 <yyj11kr@naver.com>
2024-10-03 17:22:28 +0900

    chore: chormatic으로 배포하기

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-// 스토리북 배포 디버깅 용
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.node = {
-        __dirname: true, // 서버에서만 __dirname 활성화
-      };
-    }
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
