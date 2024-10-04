66ab914e1e7ed42d7711655556e620d9feba66f2
iamgyu <iamgyu@naver.com>
2024-10-04 15:10:05 +0900

    refactor: 파일 이름 컨벤션에 맞게 수정 (#60)

--- app/(header)/club/member/page.tsx
+++ app/(header)/club/member/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import OneMemberInfo from "./OneMemberInfo";
+import OneMemberInfo from "./MemberInfo";
 
 function ClubMemberPage() {
   const [openDropdownIndex, setOpenDropdownIndex] = useState<number | null>(
