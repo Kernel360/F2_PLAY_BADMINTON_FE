fa9cc2998307f73a8c04e150c4f228c13afdae53
Yejin0O0 <yyj11kr@naver.com>
2024-10-07 15:28:30 +0900

    chore: fix biome error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function Page() {
   //TODO(Yejin0O0): 로그인 구현 후 redirect 분기 처리 해야 함
-  redirect('/club/intro');
+  redirect("/club/intro");
 }
