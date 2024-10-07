9330aab970e598013472444a41a89ca7c001f6af
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-07 15:31:48 +0900

    feat: root page redirect 기능 추가 (#73)
    * feat: root page redirect 기능 추가
    
    * chore: redirect url 변경
    
    * chore: fix biome error

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,6 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import DayCell from "@/components/DayCell";
-import { Calendar } from "@/components/ui/calendar";
-import { ko } from "date-fns/locale";
-import * as React from "react";
-
-export default function CalendarDemo() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
-
-  return (
-    <div>
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        locale={ko}
-        className="rounded-md text-gray-800"
-        classNames={{
-          head_cell: "w-[80px]",
-          cell: "h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center hover:bg-primary/10",
-          day: "hover:bg-inherit hover:text-inherit h-[80px] w-[80px] flex justify-center items-center",
-          day_selected:
-            "w-full border border-primary bg-inherit text-primary hover:bg-inherit hover:text-inherit",
-          day_today: "hover: bg-inherit",
-        }}
-        components={{
-          DayContent: DayCell,
-        }}
-      />
-    </div>
-  );
+export default function Page() {
+  //TODO(Yejin0O0): 로그인 구현 후 redirect 분기 처리 해야 함
+  redirect("/club/intro");
 }
