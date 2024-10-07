1265cb05b8ad77f71b2dd709f00db2c1ede60b6f
Yejin0O0 <yyj11kr@naver.com>
2024-10-07 17:40:26 +0900

    chore: fix biome error

--- constants/loginOauthUrl.ts
+++ constants/loginOauthUrl.ts
@@ -1,8 +1,8 @@
 export const NAVER_OAUTH_URL =
-  'https://badminton.run/oauth2/authorization/naver';
+  "https://badminton.run/oauth2/authorization/naver";
 
 export const GOOGLE_OAUTH_URL =
-  'https://badminton.run/oauth2/oauth2/authorization/google';
+  "https://badminton.run/oauth2/oauth2/authorization/google";
 
 export const KAKAO_OAUTH_URL =
-  'https://badminton.run/oauth2/oauth2/authorization/kakao';
+  "https://badminton.run/oauth2/oauth2/authorization/kakao";
