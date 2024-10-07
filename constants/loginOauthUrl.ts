fc6ff6225c912bb8a64945a80b3813636fcc3f63
Yejin0O0 <yyj11kr@naver.com>
2024-10-07 17:28:55 +0900

    feat: https 환경 세팅

--- constants/loginOauthUrl.ts
+++ constants/loginOauthUrl.ts
@@ -1,8 +1,8 @@
 export const NAVER_OAUTH_URL =
-  "http://3.38.247.217:8080/oauth2/authorization/naver";
+  'https://badminton.run/oauth2/authorization/naver';
 
 export const GOOGLE_OAUTH_URL =
-  "http://3.38.247.217:8080/oauth2/authorization/google";
+  'https://badminton.run/oauth2/oauth2/authorization/google';
 
 export const KAKAO_OAUTH_URL =
-  "http://3.38.247.217:8080/oauth2/authorization/kakao";
+  'https://badminton.run/oauth2/oauth2/authorization/kakao';
