516c85bdcccf09690a1f1f198cae8b41f54ecd7a
Yejin0O0 <yyj11kr@naver.com>
2024-10-07 15:05:40 +0900

    chore: 페이지 링크 연결

--- components/clubs/ClubCard.tsx
+++ components/clubs/ClubCard.tsx
@@ -6,70 +6,73 @@ import {
 } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 import { Text } from "../ui/Text";
 
 type CardProps = React.ComponentProps<typeof Card>;
 
 export function ClubCard({ className }: CardProps) {
   return (
-    <Card
-      className={cn(
-        "w-64 rounded-lg shadow-lg overflow-hidden cursor-pointer transition-transform transform hover:scale-105",
-        className,
-      )}
-    >
-      <CardHeader className="grid gap-4 p-0">
-        <Image
-          src="/images/dummy-image.jpg"
-          alt="card image"
-          width={256}
-          height={256}
-          className="w-full h-48 object-cover"
-        />
-      </CardHeader>
-      <CardContent className="py-4 px-6 h-[80px]">
-        <Text className=" font-semibold" lineClamp={2}>
-          Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360 프론트엔드 배드민턴
-          동호회 Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360 프론트엔드
-          배드민턴 동호회 Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360
-          프론트엔드 배드민턴 동호회
-        </Text>
-      </CardContent>
-      <CardFooter className="pb-2">
-        <div className="w-full flex justify-around border-t border-solid border-gray-200 py-2">
-          <div className="flex items-center">
-            <Image
-              src="/images/tier-gold.png"
-              alt="tier-gold"
-              width={20}
-              height={20}
-              className="pr-1"
-            />
-            <Text className="text-sm">8 명</Text>
+    <Link href="/club">
+      <Card
+        className={cn(
+          "w-64 rounded-lg shadow-lg overflow-hidden cursor-pointer transition-transform transform hover:scale-105",
+          className,
+        )}
+      >
+        <CardHeader className="grid gap-4 p-0">
+          <Image
+            src="/images/dummy-image.jpg"
+            alt="card image"
+            width={256}
+            height={256}
+            className="w-full h-48 object-cover"
+          />
+        </CardHeader>
+        <CardContent className="py-4 px-6 h-[80px]">
+          <Text className=" font-semibold" lineClamp={2}>
+            Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360 프론트엔드 배드민턴
+            동호회 Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360 프론트엔드
+            배드민턴 동호회 Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360
+            프론트엔드 배드민턴 동호회
+          </Text>
+        </CardContent>
+        <CardFooter className="pb-2">
+          <div className="w-full flex justify-around border-t border-solid border-gray-200 py-2">
+            <div className="flex items-center">
+              <Image
+                src="/images/tier-gold.png"
+                alt="tier-gold"
+                width={20}
+                height={20}
+                className="pr-1"
+              />
+              <Text className="text-sm">8 명</Text>
+            </div>
+            <div className="flex items-center">
+              <Image
+                src="/images/tier-silver.png"
+                alt="tier-silver"
+                width={20}
+                height={20}
+                className="pr-1"
+              />
+              <Text className="text-sm">10 명</Text>
+            </div>
+            <div className="flex items-center">
+              <Image
+                src="/images/tier-bronze.png"
+                alt="tier-bronze"
+                width={20}
+                height={20}
+                className="pr-1"
+              />
+              <Text className="text-sm">14 명</Text>
+            </div>
           </div>
-          <div className="flex items-center">
-            <Image
-              src="/images/tier-silver.png"
-              alt="tier-silver"
-              width={20}
-              height={20}
-              className="pr-1"
-            />
-            <Text className="text-sm">10 명</Text>
-          </div>
-          <div className="flex items-center">
-            <Image
-              src="/images/tier-bronze.png"
-              alt="tier-bronze"
-              width={20}
-              height={20}
-              className="pr-1"
-            />
-            <Text className="text-sm">14 명</Text>
-          </div>
-        </div>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
 
