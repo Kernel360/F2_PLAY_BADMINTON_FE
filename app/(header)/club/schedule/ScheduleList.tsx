fefdf8c4c4c8dfc10ade55772d212053f53fcbbb
Yejin0O0 <yyj11kr@naver.com>
2024-10-07 14:44:19 +0900

    chore: fix biome error

--- app/(header)/club/schedule/ScheduleList.tsx
+++ app/(header)/club/schedule/ScheduleList.tsx
@@ -1,6 +1,6 @@
 import { Text } from "@/components/ui/Text";
-import Link from "next/link";
 import { format } from "date-fns";
+import Link from "next/link";
 import React from "react";
 
 const schedules = [
