76bc00763c46ac70a7eb5fe970f9a29455da4b3c
Yejin0O0 <yyj11kr@naver.com>
2024-10-08 15:40:07 +0900

    chore: provider 네이밍 변경

--- lib/TanstackClientProvider.tsx
+++ lib/TanstackClientProvider.tsx
@@ -4,13 +4,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type React from "react";
 
-type ProvidersProps = {
+type TanstackClientProviderProps = {
   children: React.ReactNode;
 };
 
 const queryClient = new QueryClient();
 
-export function Providers({ children }: ProvidersProps) {
+export function TanstackClientProvider({
+  children,
+}: TanstackClientProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
