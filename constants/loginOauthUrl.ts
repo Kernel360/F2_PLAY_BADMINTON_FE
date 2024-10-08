bcbb0815c58f1a947332692d77a8da3e8f261885
Yejin0O0 <yyj11kr@naver.com>
2024-10-08 10:07:44 +0900

    chore: 요청 url 변경

--- constants/loginOauthUrl.ts
+++ constants/loginOauthUrl.ts
@@ -1,8 +1,8 @@
 export const NAVER_OAUTH_URL =
-  "https://badminton.run/oauth2/authorization/naver";
+  "http://api.badminton.run/oauth2/authorization/naver";
 
 export const GOOGLE_OAUTH_URL =
-  "https://badminton.run/oauth2/oauth2/authorization/google";
+  "https://api.badminton.run/oauth2/oauth2/authorization/google";
 
 export const KAKAO_OAUTH_URL =
-  "https://badminton.run/oauth2/oauth2/authorization/kakao";
+  "https://api.badminton.run/oauth2/oauth2/authorization/kakao";
