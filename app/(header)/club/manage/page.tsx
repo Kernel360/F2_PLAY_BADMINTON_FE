d8f5d1dbee4e0dbc5deded59550133c6f42837ef
iamgyu <iamgyu@naver.com>
2024-10-08 12:49:44 +0900

    design: 동호회 관리 페이지 UI 변경

--- app/(header)/club/manage/page.tsx
+++ app/(header)/club/manage/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/Button";
 import type React from "react";
 import { useRef, useState } from "react";
 
 function ClubManagePage() {
   const [imagePreview, setImagePreview] = useState("/images/dummy-image.jpg");
+  const [clubName, setClubName] = useState("기존 동호회 이름");
+  const [text, setText] = useState(
+    "안녕하세요 동호회 입니다. 안녕하세요 동호회 입니다.안녕하세요 동호회입니다. 안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회 입니다.안녕하세요동호회 입니다. 안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회 입니다.",
+  );
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,50 +21,65 @@ function ClubManagePage() {
     }
   };
 
+  const handleImageDelete = () => {
+    setImagePreview("/images/dummy-image.jpg");
+  };
+
   const handleImageClick = () => {
     fileInputRef.current?.click();
   };
 
   return (
-    <div className="flex flex-col justify-center items-center gap-8">
+    <div className="relative flex flex-col justify-center items-center gap-4">
+      <Button className="absolute right-0 top-0">변경 저장</Button>
       <div className="flex space-x-8 w-full">
-        <div className="flex flex-col items-center gap-4">
-          <form>
-            <button
-              type="button"
-              onClick={handleImageClick}
-              className="focus:outline-none"
-            >
+        <div className="flex flex-col w-full">
+          <div className="flex items-center">
+            <div className="flex flex-col items-center gap-2">
+              <p className="text-gray-400">동호회 사진</p>
               <img
                 src={imagePreview}
                 alt="preview"
-                className="object-cover w-96 h-80 rounded-md"
+                className="object-cover w-80 h-56 rounded-md"
               />
-            </button>
+            </div>
+            <div className="flex ml-8 gap-4">
+              <div>
+                <Button onClick={handleImageClick}>사진 교체</Button>
+                <input
+                  type="file"
+                  className="hidden"
+                  ref={fileInputRef}
+                  onChange={handleImageChange}
+                />
+              </div>
+              <Button
+                onClick={handleImageDelete}
+                className="border border-red-500 text-red-500 bg-white hover:bg-white"
+              >
+                사진 삭제
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 mt-4">
+            <p className="text-gray-400">동호회 이름</p>
             <input
-              type="file"
-              className="hidden"
-              ref={fileInputRef}
-              onChange={handleImageChange}
+              type="text"
+              className="border border-gray-400 rounded-md text-full px-1 py-2 text-black outline-none"
+              value={clubName}
+              onChange={(e) => setClubName(e.target.value)}
             />
-          </form>
-          <input
-            type="text"
-            className="rounded-md w-full text-black"
-            placeholder="기존 동호회 이름"
-          />
+          </div>
+          <div className="flex flex-col gap-2 mt-4">
+            <p className="text-gray-400">동호회 소개</p>
+            <textarea
+              className="border border-gray-400 rounded-md w-full p-1 outline-none text-black overflow:scroll resize-none"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
         </div>
-        <textarea
-          placeholder="기존 소개글"
-          className="rounded-md flex-1 outline-none text-black overflow:scroll placeholder-gray-400"
-        />
       </div>
-      <button
-        type="button"
-        className="px-8 py-3 bg-primary rounded-md font-bold"
-      >
-        수정
-      </button>
     </div>
   );
 }
