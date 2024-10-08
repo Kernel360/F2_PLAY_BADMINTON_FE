87efa26a6d0a1f2d66cd19c6ac7661fe8bb5800d
iamgyu <iamgyu@naver.com>
2024-10-08 16:00:02 +0900

    design: 동호회 관리 페이지 UI 변경 (#89)
    * design: 동호회 관리 페이지 UI 변경
    
    * design: 동호회 관리 페이지 UI 변경
    
    * refactor: 동호회 수정 페이지 컴포넌트화
    
    * refactor: 안쓰는 import 제거
    
    * refactor: 리팩토링 세분화
    
    * refactor: 파일 네이밍 변경

---
+++ components/common/clubInfoInput/ClubInfoInputName.tsx
@@ -0,0 +1,17 @@
+interface ClubInfoInputNameProps {
+  clubName: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const ClubInfoInputName = ({ clubName, onChange }: ClubInfoInputNameProps) => {
+  return (
+    <input
+      type="text"
+      className="text-3xl font-bold text-black border border-gray-300 rounded-md p-1"
+      value={clubName}
+      onChange={onChange}
+    />
+  );
+};
+
+export default ClubInfoInputName;
