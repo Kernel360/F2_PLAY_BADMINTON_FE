b1b0e8c844b4a1abbf0c2f41e590c45da2064fa3
iamgyu <iamgyu@naver.com>
2024-10-08 16:00:02 +0900

    design: 동호회 관리 페이지 UI 변경 (#89)
    * design: 동호회 관리 페이지 UI 변경
    
    * design: 동호회 관리 페이지 UI 변경
    
    * refactor: 동호회 수정 페이지 컴포넌트화
    
    * refactor: 안쓰는 import 제거
    
    * refactor: 리팩토링 세분화
    
    * refactor: 파일 네이밍 변경

--- app/(header)/club/manage/page.tsx
+++ app/(header)/club/manage/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import type React from "react";
-import { useRef, useState } from "react";
+import ClubInfoInputDescription from "@/components/common/clubInfoInput/ClubInfoInputDescription";
+import ClubInfoInputImage from "@/components/common/clubInfoInput/ClubInfoInputImage";
+import ClubInfoInputName from "@/components/common/clubInfoInput/ClubInfoInputName";
+import { Button } from "@/components/ui/Button";
+import { useState } from "react";
 
 function ClubManagePage() {
   const [imagePreview, setImagePreview] = useState("/images/dummy-image.jpg");
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [clubName, setClubName] = useState("기존 동호회 이름");
+  const [text, setText] = useState(
+    "안녕하세요 동호회 입니다. 안녕하세요 동호회 입니다.안녕하세요 동호회입니다. 안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회 입니다.안녕하세요동호회 입니다. 안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회입니다.안녕하세요 동호회 입니다. 안녕하세요 동호회 입니다.",
+  );
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -16,50 +22,25 @@ function ClubManagePage() {
     }
   };
 
-  const handleImageClick = () => {
-    fileInputRef.current?.click();
-  };
-
   return (
-    <div className="flex flex-col justify-center items-center gap-8">
-      <div className="flex space-x-8 w-full">
-        <div className="flex flex-col items-center gap-4">
-          <form>
-            <button
-              type="button"
-              onClick={handleImageClick}
-              className="focus:outline-none"
-            >
-              <img
-                src={imagePreview}
-                alt="preview"
-                className="object-cover w-96 h-80 rounded-md"
-              />
-            </button>
-            <input
-              type="file"
-              className="hidden"
-              ref={fileInputRef}
-              onChange={handleImageChange}
-            />
-          </form>
-          <input
-            type="text"
-            className="rounded-md w-full text-black"
-            placeholder="기존 동호회 이름"
-          />
-        </div>
-        <textarea
-          placeholder="기존 소개글"
-          className="rounded-md flex-1 outline-none text-black overflow:scroll placeholder-gray-400"
+    <div className="flex space-x-8 w-full h-[464px] items-center">
+      <ClubInfoInputImage
+        imagePreview={imagePreview}
+        onImageChange={handleImageChange}
+      />
+      <div className="flex flex-col flex-1 h-[400px] gap-4">
+        <ClubInfoInputName
+          clubName={clubName}
+          onChange={(e) => setClubName(e.target.value)}
         />
+        <ClubInfoInputDescription
+          description={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="w-full flex justify-end">
+          <Button className="place-items-end">변경 저장</Button>
+        </div>
       </div>
-      <button
-        type="button"
-        className="px-8 py-3 bg-primary rounded-md font-bold"
-      >
-        수정
-      </button>
     </div>
   );
 }
