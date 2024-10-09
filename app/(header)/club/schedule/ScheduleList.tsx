3ae8ef62afb0275aecd000b357dc8c45e2d15fd7
Yejin0O0 <yyj11kr@naver.com>
2024-10-09 18:58:52 +0900

    chore: 안쓰는 코드 제거

--- app/(header)/club/schedule/ScheduleList.tsx
+++ app/(header)/club/schedule/ScheduleList.tsx
@@ -89,24 +89,6 @@ const schedules = [
   },
 ];
 
-const plusIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    className="lucide lucide-plus text-black hover:text-white"
-  >
-    <path d="M5 12h14" />
-    <path d="M12 5v14" />
-    <title>plus</title>
-  </svg>
-);
 interface ScheduleListProps {
   selectedDate: Date;
 }
