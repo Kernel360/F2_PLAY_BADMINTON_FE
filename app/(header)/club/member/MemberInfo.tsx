367f97751de00b109e6091160529341c6b5245cd
iamgyu <iamgyu@naver.com>
2024-10-10 16:53:40 +0900

    design: 멤버 페이지 UI 작업 (#94)
    * design: 멤버 페이지 UI 작업
    
    * design: 멤버 페이지 UI 변경
    
    * design: 티어 글자색 변경
    
    * feat: 역할 변경 모달 구현
    
    * feat: 정지 모달 구현
    
    * refactor: 모달창 기능 수정
    
    * refactor: dropdown 단어 수정
    
    * refactor: 파일 네이밍 변경
    
    * refactor: interface 요소 변수 선언

--- app/(header)/club/member/MemberInfo.tsx
+++ app/(header)/club/member/MemberInfo.tsx
@@ -1,33 +1,50 @@
 "use client";
 
 import IconButton from "@/components/ui/IconButton";
+import { getTierWithEmoji } from "@/utils/getTierWithEmoji";
 import { AlignJustify } from "lucide-react";
 import React from "react";
 import MemberDropDown from "./MemberDropdown";
 
-interface OneMemberInfoProps {
+interface LeagueRecordInfoResponse {
+  winCount: number;
+  loseCount: number;
+  drawCount: number;
+  matchCount: number;
+  tier: string;
+}
+
+interface MemberInfoProps {
+  image: string;
+  name: string;
+  leagueRecordInfoResponse: LeagueRecordInfoResponse;
   isOpen: boolean; // 드롭다운 상태
   onToggle: () => void; // 드롭다운 토글 함수
 }
 
-function OneMemberInfo({ isOpen, onToggle }: OneMemberInfoProps) {
+function MemberInfo({
+  image,
+  name,
+  leagueRecordInfoResponse,
+  isOpen,
+  onToggle,
+}: MemberInfoProps) {
+  const { winCount, loseCount, drawCount, matchCount, tier } =
+    leagueRecordInfoResponse;
+
   return (
-    <div className="flex justify-center space-x-40">
-      <div className="flex items-center gap-8">
-        <div className="w-20 h-20">
-          <img
-            src="/images/dummy-image.jpg"
-            alt="member_image"
-            className=" object-cover w-full h-full rounded-full"
-          />
-        </div>
-        <div className="w-8 h-8">
-          <img src="/images/tier-gold.png" alt="member_tier" />
-        </div>
-        <p className="text-gray-400">멤버 이름</p>
+    <div className="flex justify-center">
+      <div className="flex flex-[1] items-center gap-8">
+        <img src={image} alt="userImg" className="w-16 h-16 rounded-full" />
+        <p className="text-black">{name}</p>
       </div>
-      <div className="flex items-center gap-8 relative">
-        <p className="text-gray-400">00전 | 00승 | 00무 | 00패</p>
+      <div className="flex flex-[1] items-center">
+        <p className="text-black">{getTierWithEmoji(tier)}</p>
+      </div>
+      <div className="flex flex-[1] items-center justify-between relative">
+        <p className="text-black">
+          {matchCount}전 | {winCount}승 | {drawCount}무 | {loseCount}패
+        </p>
         <IconButton size="sm" color="transparent" onClick={onToggle}>
           <AlignJustify width="80%" height="80%" className="text-gray-400" />
         </IconButton>
@@ -37,4 +54,4 @@ function OneMemberInfo({ isOpen, onToggle }: OneMemberInfoProps) {
   );
 }
 
-export default OneMemberInfo;
+export default MemberInfo;
