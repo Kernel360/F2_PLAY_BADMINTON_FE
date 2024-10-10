6421187c9a652d9b0e2cd79cbb0f302f93da396f
iamgyu <iamgyu@naver.com>
2024-10-10 16:53:40 +0900

    design: 멤버 페이지 UI 작업 (#94)
    * design: 멤버 페이지 UI 작업
    
    * design: 멤버 페이지 UI 변경
    
    * design: 티어 글자색 변경
    
    * feat: 역할 변경 모달 구현
    
    * feat: 정지 모달 구현
    
    * refactor: 모달창 기능 수정
    
    * refactor: dropdown 단어 수정
    
    * refactor: 파일 네이밍 변경
    
    * refactor: interface 요소 변수 선언

---
+++ app/(header)/club/member/MemberRollModal.tsx
@@ -0,0 +1,73 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import type React from "react";
+import { useState } from "react";
+
+interface MemberRollChangeProps {
+  openRollChange: boolean;
+  handleRollDialog: () => void;
+}
+
+function MemberRollChange({
+  openRollChange,
+  handleRollDialog,
+}: MemberRollChangeProps) {
+  const [selectedRole, setSelectedRole] = useState("");
+
+  const handleRoleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedRole(e.target.value);
+  };
+
+  const roles = [
+    {
+      id: 1,
+      name: "회장",
+    },
+    {
+      id: 2,
+      name: "매니저",
+    },
+    {
+      id: 3,
+      name: "멤버",
+    },
+  ];
+
+  return (
+    <Dialog open={openRollChange} onOpenChange={handleRollDialog}>
+      <DialogContent className="text-black">
+        <DialogHeader>
+          <DialogTitle>멤버 역할 교체</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col w-full items-center">
+          <div className="flex pb-4 gap-8">
+            {roles.map((role) => (
+              <div key={role.id} className="mb-2">
+                <label className="flex items-center">
+                  <input
+                    type="radio"
+                    value={role.name}
+                    checked={selectedRole === role.name}
+                    onChange={handleRoleChange}
+                    className="mr-2"
+                  />
+                  {role.name}
+                </label>
+              </div>
+            ))}
+          </div>
+          <DialogClose className="bg-primary text-white rounded-md px-6 py-2">
+            교체
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default MemberRollChange;
