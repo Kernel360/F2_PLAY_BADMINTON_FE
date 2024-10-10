31542dbd3a840bc6ed6956495d43ba1f1197f013
iamgyu <iamgyu@naver.com>
2024-10-10 16:53:26 +0900

    design: 동호회 생성 페이지 UI 작업 (#95)
    * design: 동호회 생성 페이지 UI 작업
    
    * refactor: 코멘트 내용 반영

--- app/(header)/createClub/page.tsx
+++ app/(header)/createClub/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import ClubInfoInputDescription from "@/components/common/clubInfoInput/ClubInfoInputDescription";
+import ClubInfoInputImage from "@/components/common/clubInfoInput/ClubInfoInputImage";
+import ClubInfoInputName from "@/components/common/clubInfoInput/ClubInfoInputName";
 import { Button } from "@/components/ui/Button";
 import type React from "react";
 import { useRef, useState } from "react";
 
 function CreateClubPage() {
   const [imagePreview, setImagePreview] = useState("/images/dummy-image.jpg");
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [clubName, setClubName] = useState("");
+  const [text, setText] = useState("");
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -17,58 +21,37 @@ function CreateClubPage() {
     }
   };
 
-  const handleImageClick = () => {
-    fileInputRef.current?.click();
-  };
-
   return (
     <div className="px-14 pt-8 w-full">
-      <div className="flex flex-col gap-4">
-        <p className="text-gray-400">동호회 이름</p>
-        <input
-          type="text"
-          className="w-full text-gray-400 text-2xl bg-transparent outline-none border-gray-300 border-b-[1px] focus:border-gray-800"
-          placeholder="동호회 이름을 작성해주세요"
+      <div className="flex space-x-8 w-full h-[464px] items-center">
+        <ClubInfoInputImage
+          imagePreview={imagePreview}
+          onImageChange={handleImageChange}
         />
-      </div>
-      <div className=" w-full pt-8 flex flex-col gap-4 text-gray-400">
-        <p>동호회 사진</p>
-        <div>
-          <form className="border-gray-400 rounded-md h-[400px] w-[400px]">
-            <button
-              type="button"
-              className="h-full w-full"
-              onClick={handleImageClick}
-            >
-              <img
-                alt="previewImg"
-                src={imagePreview}
-                className="object-contain border-2 border-gray-400 rounded-md w-full h-full"
-              />
-            </button>
-            <input
-              type="file"
-              className="hidden"
-              ref={fileInputRef}
-              onChange={handleImageChange}
+        <div className="flex flex-col flex-1 h-[400px] gap-4">
+          <div className="flex flex-col gap-1">
+            <p className="text-black font-bold text-lg">동호회 이름</p>
+            <ClubInfoInputName
+              clubName={clubName}
+              onChange={(e) => setClubName(e.target.value)}
             />
-          </form>
+          </div>
+
+          <div className="flex flex-col gap-1 h-full">
+            <p className="text-black font-bold text-lg">동호회 소개</p>
+            <ClubInfoInputDescription
+              description={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
         </div>
       </div>
-      <div className="flex flex-col pt-8 gap-4">
-        <p className="text-gray-400">동호회 소개</p>
-        <textarea
-          placeholder="동호회 소개를 작성해주세요!"
-          className="w-full rounded-md border-2 border-gray-400 resize-none"
-        />
-      </div>
-      <div className="flex justify-center items-center pt-8 gap-4">
+      <div className="flex justify-center items-center gap-4">
         <Button variant="outline" size="lg">
           취소
         </Button>
         <Button size="lg">완료</Button>
       </div>
-      <div className="m-12" />
     </div>
   );
 }
