965851bbc1ca701677fba74b136885ab073bdb93
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-11 18:31:46 +0900

    feat: 동호회 전체 조회 openApi-typescript 연동 및 fetch (#104)
    * feat: 동호회 전체 조회 openApi-typescript 연동 및 fetch
    
    * chore: fix biome error

--- components/clubs/ClubCard.tsx
+++ components/clubs/ClubCard.tsx
@@ -5,25 +5,25 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import type { components } from "@/schemas/schema";
 import Link from "next/link";
 import { Text } from "../ui/Text";
 
-type CardProps = React.ComponentProps<typeof Card>;
+type ClubsData = components["schemas"]["ClubCardResponse"];
 
-export function ClubCard({ className }: CardProps) {
+export function ClubCard(props: ClubsData) {
+  const { club_name, club_image, club_member_count_by_tier: tier } = props;
   return (
     <Link href="/my-club">
       <Card
-        className={cn(
-          "w-64 rounded-lg shadow-lg overflow-hidden cursor-pointer transition-transform transform hover:scale-105",
-          className,
-        )}
+        className={
+          "w-64 rounded-lg shadow-lg overflow-hidden cursor-pointer transition-transform transform hover:scale-105"
+        }
       >
         <CardHeader className="grid gap-4 p-0">
-          <Image
-            src="/images/dummy-image.jpg"
-            alt="card image"
+          <img
+            src={club_image || "/images/dummy-image.jpg"}
+            alt="card"
             width={256}
             height={256}
             className="w-full h-48 object-cover"
@@ -31,43 +31,44 @@ export function ClubCard({ className }: CardProps) {
         </CardHeader>
         <CardContent className="py-4 px-6 h-[80px]">
           <Text className=" font-semibold" lineClamp={2}>
-            Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360 프론트엔드 배드민턴
-            동호회 Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360 프론트엔드
-            배드민턴 동호회 Kernel 360 프론트엔드 배드민턴 동호회 Kernel 360
-            프론트엔드 배드민턴 동호회
+            {club_name}
           </Text>
         </CardContent>
         <CardFooter className="pb-2">
           <div className="w-full flex justify-around border-t border-solid border-gray-200 py-2">
             <div className="flex items-center">
-              <Image
+              <img
                 src="/images/tier-gold.png"
                 alt="tier-gold"
                 width={20}
                 height={20}
                 className="pr-1"
               />
-              <Text className="text-sm">8 명</Text>
+              <Text className="text-sm">{tier?.gold_club_member_count} 명</Text>
             </div>
             <div className="flex items-center">
-              <Image
+              <img
                 src="/images/tier-silver.png"
                 alt="tier-silver"
                 width={20}
                 height={20}
                 className="pr-1"
               />
-              <Text className="text-sm">10 명</Text>
+              <Text className="text-sm">
+                {tier?.silver_club_member_count} 명
+              </Text>
             </div>
             <div className="flex items-center">
-              <Image
+              <img
                 src="/images/tier-bronze.png"
                 alt="tier-bronze"
                 width={20}
                 height={20}
                 className="pr-1"
               />
-              <Text className="text-sm">14 명</Text>
+              <Text className="text-sm">
+                {tier?.bronze_club_member_count} 명
+              </Text>
             </div>
           </div>
         </CardFooter>
