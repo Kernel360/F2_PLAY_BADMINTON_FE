07d0ca348ac451a5b7d174bd254e80542042547a
Yejin0O0 <yyj11kr@naver.com>
2024-10-14 17:07:57 +0900

    chore: 프로필 사진 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "badminton-team.s3.ap-northeast-2.amazonaws.com"],
+    domains: [
+      "localhost",
+      "badminton-team.s3.ap-northeast-2.amazonaws.com",
+      "ssl.pstatic.net", // 네이버 프로필 사진
+    ],
   },
 };
 
