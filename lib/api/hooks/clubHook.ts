69225d5867a5a14e61c706e33728e5caed635f3f
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-14 11:11:57 +0900

    feat: 동호회 생성 api 연동 및 유효성 검증 (#105)
    * refactor: useClubs 화살표 함수 변경
    
    * feat: 동호회 생성 페이지 API 연동 완료
    
    * feat: 동호회 생성 에러 처리
    
    * feat: 동호회 생성 Fetch
    
    * feat: 동호회 생성 api 연결
    
    * chore: 함수 분리
    
    * fix: 업로드 된 이미지 url로만 preview 설정
    
    * feat: 유효성 검증 로직 추가
    
    * chore: clubs 삭제
    
    * chore: 온점 제거
    
    * chore: 유효성 검사 수정
    
    * chore: next config 수정
    
    * chore: tsc 에러 해결
    
    ---------
    
    Co-authored-by: iamgyu <iamgyu@naver.com>

--- lib/api/hooks/clubHook.ts
+++ lib/api/hooks/clubHook.ts
@@ -1,9 +1,31 @@
-import { getClubs } from "@/lib/api/functions/clubFn";
-import { useQuery } from "@tanstack/react-query";
+import { getClubs, postClubs, postClubsImg } from "@/lib/api/functions/clubFn";
+import type { components } from "@/schemas/schema";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export default function useClubs() {
+type ClubCreate = components["schemas"]["ClubCreateRequest"];
+
+export const useGetClubs = () => {
   return useQuery({
     queryKey: ["clubsData"],
     queryFn: getClubs,
   });
-}
+};
+
+export const usePostClubs = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (clubData: ClubCreate) => postClubs(clubData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["clubsData"] });
+    },
+    onError: (error: Error) => alert(error),
+  });
+};
+
+export const usePostClubsImg = () => {
+  return useMutation<string, Error, FormData>({
+    mutationFn: (clubImg: FormData) => postClubsImg(clubImg),
+    onError: (error: Error) => alert(error),
+  });
+};
