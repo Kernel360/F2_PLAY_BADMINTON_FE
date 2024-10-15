7f1621f833697b1e0654d4d6d3ddf3ef71cdba51
iamgyu <iamgyu@naver.com>
2024-10-15 16:12:09 +0900

    refactor: 사용하지 않는 import 제거

--- app/(header)/club/[clubId]/page.tsx
+++ app/(header)/club/[clubId]/page.tsx
@@ -1,8 +1,3 @@
-import ClubIntroPages from "@/app/(header)/club/[clubId]/ClubIntroPage";
-import ClubManagePages from "@/app/(header)/club/[clubId]/manage/page";
-import ClubMemberPages from "@/app/(header)/club/[clubId]/member/page";
-import ClubSchedulePages from "@/app/(header)/club/[clubId]/schedule/page";
-import { TabsContent } from "@/components/ui/Tabs";
 import ClubLayout from "./layout";
 
 function MyClubPage() {
