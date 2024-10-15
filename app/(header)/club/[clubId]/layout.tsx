e1feea9a85e3bfc0697fb9da0ded019091ac3d2c
iamgyu <iamgyu@naver.com>
2024-10-15 16:40:18 +0900

    refactor: TODO 추가 및 구조 복구

--- app/(header)/club/[clubId]/layout.tsx
+++ app/(header)/club/[clubId]/layout.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type React from "react";
 
+/* TODO: 페이지와 관련된 코드 리팩토링 필요 */
 function ClubLayout() {
   const currentPath = usePathname();
   const clubId = currentPath.split("/")[2];
@@ -32,60 +33,53 @@ function ClubLayout() {
     return pathname;
   };
 
-  const TabTriggers = () => (
-    <TabsList>
-      <Link href={`/club/${clubId}`}>
-        <TabsTrigger value="intro" color="gray">
-          소개
-        </TabsTrigger>
-      </Link>
-      {isMember && (
-        <>
-          <Link href={`/club/${clubId}/schedule`}>
-            <TabsTrigger value="schedule" color="gray">
-              일정
-            </TabsTrigger>
-          </Link>
-          <Link href={`/club/${clubId}/member`}>
-            <TabsTrigger value="member" color="gray">
-              회원
-            </TabsTrigger>
-          </Link>
-          <Link href={`/club/${clubId}/manage`}>
-            <TabsTrigger value="manage" color="gray">
-              관리
-            </TabsTrigger>
-          </Link>
-        </>
-      )}
-    </TabsList>
-  );
-
-  const TabContents = () => (
-    <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow flex-1 space-y-4 p-8 min-h-[530px]">
-      <TabsContent value="intro">
-        <ClubIntroPages />
-      </TabsContent>
-      {isMember && (
-        <>
-          <TabsContent value="schedule">
-            <ClubSchedulePages />
-          </TabsContent>
-          <TabsContent value="member">
-            <ClubMemberPages />
-          </TabsContent>
-          <TabsContent value="manage">
-            <ClubManagePages />
-          </TabsContent>
-        </>
-      )}
-    </div>
-  );
-
+  /* TODO: 겹치는 컴포넌트 분리 필요 */
   return (
     <Tabs defaultValue={activeTab()} className="w-full max-w-5xl mt-10">
-      <TabTriggers />
-      <TabContents />
+      <TabsList>
+        <Link href={`/club/${clubId}`}>
+          <TabsTrigger value="intro" color="gray">
+            소개
+          </TabsTrigger>
+        </Link>
+        {isMember && (
+          <>
+            <Link href={`/club/${clubId}/schedule`}>
+              <TabsTrigger value="schedule" color="gray">
+                일정
+              </TabsTrigger>
+            </Link>
+            <Link href={`/club/${clubId}/member`}>
+              <TabsTrigger value="member" color="gray">
+                회원
+              </TabsTrigger>
+            </Link>
+            <Link href={`/club/${clubId}/manage`}>
+              <TabsTrigger value="manage" color="gray">
+                관리
+              </TabsTrigger>
+            </Link>
+          </>
+        )}
+      </TabsList>
+      <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow flex-1 space-y-4 p-8 min-h-[530px]">
+        <TabsContent value="intro">
+          <ClubIntroPages />
+        </TabsContent>
+        {isMember && (
+          <>
+            <TabsContent value="schedule">
+              <ClubSchedulePages />
+            </TabsContent>
+            <TabsContent value="member">
+              <ClubMemberPages />
+            </TabsContent>
+            <TabsContent value="manage">
+              <ClubManagePages />
+            </TabsContent>
+          </>
+        )}
+      </div>
     </Tabs>
   );
 }
