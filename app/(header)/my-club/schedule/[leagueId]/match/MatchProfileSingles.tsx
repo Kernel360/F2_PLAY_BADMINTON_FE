fc119cbe23de97a94a9823cf9b770a1fdd46d454
iamgyu <iamgyu@naver.com>
2024-10-15 11:34:39 +0900

    feat: 대진표 화면 및 기능 구현 (#112)
    * feat: 대진표 화면 및 기능 구현
    
    * refactor: 코멘트 수정

---
+++ app/(header)/my-club/schedule/[leagueId]/match/MatchProfileSingles.tsx
@@ -0,0 +1,61 @@
+import MatchScoreModalSingles from "./MatchScoreModalSingles";
+
+interface MatchProfileSinglesProps {
+  singlesMatch: {
+    participant1_name: string;
+    participant1_image: string;
+    participant2_name: string;
+    participant2_image: string;
+  };
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function MatchProfileSingles({
+  singlesMatch,
+  isOpen,
+  onClose,
+}: MatchProfileSinglesProps) {
+  const {
+    participant1_name,
+    participant1_image,
+    participant2_name,
+    participant2_image,
+  } = singlesMatch;
+
+  return (
+    <div className="flex">
+      <div className="flex rounded-md bg-gray-200 items-center w-72 p-2 justify-between">
+        <div className="flex flex-col items-center gap-4">
+          <img
+            src={participant1_image}
+            alt="user"
+            className="h-20 w-20 rounded-full"
+          />
+          <p className="text-black">{participant1_name}</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-sm text-black">진행 완료</p>
+          <p className="text-black text-2xl font-bold">2 vs 0</p>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <img
+            src={participant2_image}
+            alt="user"
+            className="h-20 w-20 rounded-full"
+          />
+          <p className="text-black">{participant2_name}</p>
+        </div>
+      </div>
+      {isOpen && (
+        <MatchScoreModalSingles
+          isOpen={isOpen}
+          singlesMatch={singlesMatch}
+          onClose={onClose}
+        />
+      )}
+    </div>
+  );
+}
+
+export default MatchProfileSingles;
---
+++ app/(header)/my-club/schedule/[leagueId]/match/page.tsx
@@ -0,0 +1,230 @@
+"use client";
+
+import { Button } from "@/components/ui/Button";
+import { useState } from "react";
+import MatchProfileDoubles from "./MatchProfileDoubles";
+import MatchProfileSingles from "./MatchProfileSingles";
+
+const matches = [
+  {
+    id: 1,
+    matchType: "SINGLES",
+    singlesMatch: {
+      participant1_name: "name1",
+      participant1_image: "/images/dummy-image.jpg",
+      participant2_name: "name2",
+      participant2_image: "/images/dummy-image.jpg",
+    },
+  },
+  {
+    id: 2,
+    matchType: "SINGLES",
+    singlesMatch: {
+      participant1_name: "name3",
+      participant1_image: "/images/dummy-image.jpg",
+      participant2_name: "name4",
+      participant2_image: "/images/dummy-image.jpg",
+    },
+  },
+  {
+    id: 3,
+    matchType: "SINGLES",
+    singlesMatch: {
+      participant1_name: "name5",
+      participant1_image: "/images/dummy-image.jpg",
+      participant2_name: "name6",
+      participant2_image: "/images/dummy-image.jpg",
+    },
+  },
+  {
+    id: 4,
+    matchType: "SINGLES",
+    singlesMatch: {
+      participant1_name: "name7",
+      participant1_image: "/images/dummy-image.jpg",
+      participant2_name: "name8",
+      participant2_image: "/images/dummy-image.jpg",
+    },
+  },
+  {
+    id: 5,
+    matchType: "SINGLES",
+    singlesMatch: {
+      participant1_name: "name9",
+      participant1_image: "/images/dummy-image.jpg",
+      participant2_name: "name10",
+      participant2_image: "/images/dummy-image.jpg",
+    },
+  },
+  {
+    id: 6,
+    matchType: "DOUBLES",
+    doublesMatch: {
+      team1: {
+        participant1_name: "name1",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name2",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+      team2: {
+        participant1_name: "name3",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name4",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+    },
+  },
+  {
+    id: 7,
+    matchType: "DOUBLES",
+    doublesMatch: {
+      team1: {
+        participant1_name: "name1",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name2",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+      team2: {
+        participant1_name: "name3",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name4",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+    },
+  },
+  {
+    id: 8,
+    matchType: "DOUBLES",
+    doublesMatch: {
+      team1: {
+        participant1_name: "name1",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name2",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+      team2: {
+        participant1_name: "name3",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name4",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+    },
+  },
+  {
+    id: 9,
+    matchType: "DOUBLES",
+    doublesMatch: {
+      team1: {
+        participant1_name: "name1",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name2",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+      team2: {
+        participant1_name: "name3",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name4",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+    },
+  },
+  {
+    id: 10,
+    matchType: "DOUBLES",
+    doublesMatch: {
+      team1: {
+        participant1_name: "name1",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name2",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+      team2: {
+        participant1_name: "name3",
+        participant1_image: "/images/dummy-image.jpg",
+        participant2_name: "name4",
+        participant2_image: "/images/dummy-image.jpg",
+      },
+    },
+  },
+];
+
+function MatchPage() {
+  const [matchTypes, setMatchTypes] = useState(true);
+  const [matchDialog, setMatchDialog] = useState<number | null>(null);
+
+  const toggleMatchDialog = (index: number) => {
+    setMatchDialog((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  const handleMatchTypeSingles = () => {
+    setMatchTypes(true);
+  };
+
+  const handleMatchTypeDoubles = () => {
+    setMatchTypes(false);
+  };
+
+  return (
+    <div>
+      <div className="flex gap-2">
+        <Button
+          className="rounded-full h-2 py-4"
+          onClick={handleMatchTypeSingles}
+        >
+          단식
+        </Button>
+        <Button
+          className="rounded-full h-2 py-4"
+          onClick={handleMatchTypeDoubles}
+        >
+          복식
+        </Button>
+      </div>
+      {matchTypes ? (
+        <div className="flex gap-10 w-full flex-wrap mt-5">
+          {matches
+            .filter((match) => match.matchType === "SINGLES")
+            .map((match) =>
+              match.singlesMatch ? (
+                <button
+                  type="button"
+                  key={match.id}
+                  onClick={() => toggleMatchDialog(match.id)}
+                >
+                  <MatchProfileSingles
+                    singlesMatch={match.singlesMatch}
+                    isOpen={matchDialog === match.id}
+                    onClose={() => toggleMatchDialog(match.id)}
+                  />
+                </button>
+              ) : null,
+            )}
+        </div>
+      ) : (
+        <div className="flex gap-10 w-full flex-wrap mt-5">
+          {matches
+            .filter((match) => match.matchType === "DOUBLES")
+            .map((match) =>
+              match.doublesMatch ? (
+                <button
+                  type="button"
+                  key={match.id}
+                  onClick={() => toggleMatchDialog(match.id)}
+                >
+                  <MatchProfileDoubles
+                    key={match.id}
+                    team1={match.doublesMatch.team1}
+                    team2={match.doublesMatch.team2}
+                    isOpen={matchDialog === match.id}
+                    onClose={() => toggleMatchDialog(match.id)}
+                  />
+                </button>
+              ) : null,
+            )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MatchPage;
