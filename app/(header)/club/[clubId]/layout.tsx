18c6acb9818bd43a08178283f8e77ef50cc69360
iamgyu <iamgyu@naver.com>
2024-10-15 16:07:25 +0900

    feat: 동호회 가입 여부에 따른 화면 분리

--- app/(header)/club/[clubId]/layout.tsx
+++ app/(header)/club/[clubId]/layout.tsx
@@ -1,45 +1,91 @@
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/Tabs";
-import { headers } from "next/headers";
+"use client";
+
+import ClubIntroPages from "@/app/(header)/club/[clubId]/ClubIntroPage";
+import ClubManagePages from "@/app/(header)/club/[clubId]/manage/page";
+import ClubMemberPages from "@/app/(header)/club/[clubId]/member/page";
+import ClubSchedulePages from "@/app/(header)/club/[clubId]/schedule/page";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
+import { useGetClubsById } from "@/lib/api/hooks/clubHook";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import type React from "react";
 
-function ClubLayout({ children }: { children: React.ReactNode }) {
-  const headerList = headers();
-  const clubId = headerList.get("x-current-path")?.split("/")[2];
-  const pathname = headerList.get("x-current-path")?.split("/")[3];
+function ClubLayout() {
+  const currentPath = usePathname();
+  const clubId = currentPath.split("/")[2];
+  const pathname = currentPath.split("/")[3];
+  const { data, error, isLoading } = useGetClubsById(Number(clubId ?? "-1"));
+  const isMember = data?.is_club_member as boolean;
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!data) {
+    return <div>No data available</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   const activeTab = () => {
     if (pathname === undefined) return "intro";
     return pathname;
   };
 
+  const TabTriggers = () => (
+    <TabsList>
+      <Link href={`/club/${clubId}`}>
+        <TabsTrigger value="intro" color="gray">
+          소개
+        </TabsTrigger>
+      </Link>
+      {isMember && (
+        <>
+          <Link href={`/club/${clubId}/schedule`}>
+            <TabsTrigger value="schedule" color="gray">
+              일정
+            </TabsTrigger>
+          </Link>
+          <Link href={`/club/${clubId}/member`}>
+            <TabsTrigger value="member" color="gray">
+              회원
+            </TabsTrigger>
+          </Link>
+          <Link href={`/club/${clubId}/manage`}>
+            <TabsTrigger value="manage" color="gray">
+              관리
+            </TabsTrigger>
+          </Link>
+        </>
+      )}
+    </TabsList>
+  );
+
+  const TabContents = () => (
+    <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow flex-1 space-y-4 p-8 min-h-[530px]">
+      <TabsContent value="intro">
+        <ClubIntroPages />
+      </TabsContent>
+      {isMember && (
+        <>
+          <TabsContent value="schedule">
+            <ClubSchedulePages />
+          </TabsContent>
+          <TabsContent value="member">
+            <ClubMemberPages />
+          </TabsContent>
+          <TabsContent value="manage">
+            <ClubManagePages />
+          </TabsContent>
+        </>
+      )}
+    </div>
+  );
+
   return (
-    <Tabs defaultValue={activeTab()} className="w-full max-w-5xl mt-10 ">
-      <TabsList>
-        <Link href={`/club/${clubId}`}>
-          <TabsTrigger value="intro" color="gray">
-            소개
-          </TabsTrigger>
-        </Link>
-        <Link href={`/club/${clubId}/schedule`}>
-          <TabsTrigger value="schedule" color="gray">
-            일정
-          </TabsTrigger>
-        </Link>
-        <Link href={`/club/${clubId}/member`}>
-          <TabsTrigger value="member" color="gray">
-            회원
-          </TabsTrigger>
-        </Link>
-        <Link href={`/club/${clubId}/manage`}>
-          <TabsTrigger value="manage" color="gray">
-            관리
-          </TabsTrigger>
-        </Link>
-      </TabsList>
-      <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow flex-1 space-y-4 p-8 min-h-[530px]">
-        {children}
-      </div>
+    <Tabs defaultValue={activeTab()} className="w-full max-w-5xl mt-10">
+      <TabTriggers />
+      <TabContents />
     </Tabs>
   );
 }
--- app/(header)/club/[clubId]/page.tsx
+++ app/(header)/club/[clubId]/page.tsx
@@ -3,24 +3,10 @@ import ClubManagePages from "@/app/(header)/club/[clubId]/manage/page";
 import ClubMemberPages from "@/app/(header)/club/[clubId]/member/page";
 import ClubSchedulePages from "@/app/(header)/club/[clubId]/schedule/page";
 import { TabsContent } from "@/components/ui/Tabs";
+import ClubLayout from "./layout";
 
 function MyClubPage() {
-  return (
-    <>
-      <TabsContent value="intro">
-        <ClubIntroPages />
-      </TabsContent>
-      <TabsContent value="schedule">
-        <ClubSchedulePages />
-      </TabsContent>
-      <TabsContent value="member">
-        <ClubMemberPages />
-      </TabsContent>
-      <TabsContent value="manage">
-        <ClubManagePages />
-      </TabsContent>
-    </>
-  );
+  return <ClubLayout />;
 }
 
 export default MyClubPage;
