941d60e06f3136b9c644b8cca1a0a515029b3cc2
Yejin0O0 <yyj11kr@naver.com>
2024-10-16 11:44:30 +0900

    fix: 달이 바뀔 때도 fetch 하도록 수정

--- app/(header)/club/[clubId]/schedule/page.tsx
+++ app/(header)/club/[clubId]/schedule/page.tsx
@@ -7,36 +7,44 @@ import type { components } from "@/schemas/schema";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ScheduleList from "./ScheduleList";
 
 type MonthLeagues = components["schemas"]["LeagueReadResponse"];
 
 export default function ClubSchedulePage() {
   const [date, setDate] = useState<Date>(new Date());
-
   const clubId = Number(usePathname().split("/")[2]);
-  const { data: scheduleList } = useGetMonthLeagues(
+  const { data: scheduleList, refetch } = useGetMonthLeagues(
     clubId,
     format(date, "yyyy-MM"),
   );
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    refetch();
+  }, [date]);
 
   return (
     <div className="w-full flex">
       <Calendar
         mode="single"
+        showOutsideDays={false}
         selected={date}
         onSelect={(selectedDate) => {
           if (selectedDate) {
             setDate(selectedDate);
           }
         }}
+        onMonthChange={(newMonth) => {
+          setDate(newMonth);
+        }}
         locale={ko}
         className="rounded-md text-gray-800"
         classNames={{
           head_cell: "w-[80px]",
-          cell: "h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center hover:bg-primary/10",
-          day: "hover:bg-inherit hover:text-inherit h-[80px] w-[80px] flex justify-center items-center",
+          cell: "h-[80px] w-[80px] hover:rounded-md cursor-pointer flex justify-center items-center",
+          day: "hover:bg-inherit hover:text-inherit h-[80px] w-[80px] rounded-md flex justify-center items-center",
           day_selected:
             "w-full border border-primary bg-inherit text-primary hover:bg-inherit hover:text-inherit",
           day_today: "hover: bg-inherit",
