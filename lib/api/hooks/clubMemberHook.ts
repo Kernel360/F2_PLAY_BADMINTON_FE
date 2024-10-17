c987b9799006a8ddbfaf5dffb10dc425f3f9e1e3
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-17 11:03:13 +0900

    feat: 동호회 가입 api 연동 (#142)
    * feat: 동호회 가입 API 연동
    
    * feat: 로그인 성공 시 쿼리 무효화 로직 추가
    
    ---------
    
    Co-authored-by: iamgyu <iamgyu@naver.com>

--- lib/api/hooks/clubMemberHook.ts
+++ lib/api/hooks/clubMemberHook.ts
@@ -1,9 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
-import { getClubMembers } from "../functions/clubMemberFn";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getClubMembers, postClubMembers } from "../functions/clubMemberFn";
 
 export const useGetClubMembers = () => {
   return useQuery({
     queryKey: ["clubMembersData"],
     queryFn: getClubMembers,
+  });
+};
+
+export const usePostClubMembers = (clubId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => postClubMembers(clubId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["clubMembersData"] });
+      queryClient.invalidateQueries({ queryKey: ["clubsDataById"] });
+      queryClient.invalidateQueries({ queryKey: ["isClubMemberData"] });
+      queryClient.invalidateQueries({ queryKey: ["myInfo"] });
+    },
+    onError: (error: Error) => alert(error),
   });
 };
