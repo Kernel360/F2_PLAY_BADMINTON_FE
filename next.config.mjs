7493129629d63c226fc30ed1693bf85b27efdfd5
Yejin0O0 <yyj11kr@naver.com>
2024-10-18 11:24:32 +0900

    chore: 이미지 도메인 설정

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
       "badminton-team.s3.ap-northeast-2.amazonaws.com",
       "ssl.pstatic.net", // 네이버 프로필 사진
       "lh3.googleusercontent.com", // 구글 프로필 사진
+      "d36om9pjoifd2y.cloudfront.net",
     ],
   },
 };
