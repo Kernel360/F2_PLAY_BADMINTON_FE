c2f3a896cd3f58fe444bfcebc59dd4e543d53e50
Yejin0O0 <yyj11kr@naver.com>
2024-10-28 10:44:49 +0900

    chore: fix ts error

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import { ClubCard } from "@/components/clubs/ClubCard";
 import Grid from "@/components/ui/Grid";
 import { useGetClubs } from "@/lib/api/hooks/clubHook";
+import type { components } from "@/schemas/schema";
+
+type ClubCardResponse = components["schemas"]["ClubCardResponse"];
 
 export default function page() {
   const { data, isLoading } = useGetClubs();
@@ -17,7 +20,7 @@ export default function page() {
   return (
     <div className="my-10">
       <Grid columns={3} placeItems="center" spacing="lg">
-        {data?.map((club) => (
+        {data.content?.map((club: ClubCardResponse) => (
           <ClubCard key={club.club_id} {...club} />
         ))}
       </Grid>
