ddead316e5ec057006a3fe50f388c901ca77ce0d
Yejin0O0 <yyj11kr@naver.com>
2024-10-29 18:43:48 +0900

    feat: 로그인 url 환경변수 설정

--- components/pages/Login.tsx
+++ components/pages/Login.tsx
@@ -1,16 +1,25 @@
 import LoginButton from "@/components/login/LoginButton";
 import Image from "@/components/ui/Image";
-import { NAVER_OAUTH_URL } from "@/constants/loginOauthUrl";
 import brand from "@/public/images/main-logo.png";
 import Link from "next/link";
+
+const NAVER_OAUTH_URL = process.env.NEXT_NAVER_OAUTH_URL || "";
+const GOOGLE_OAUTH_URL = process.env.NEXT_NAVER_OAUTH_URL || "";
+const KAKAO_OAUTH_URL = process.env.NEXT_NAVER_OAUTH_URL || "";
 
 function Login() {
   return (
     <div className="flex flex-col justify-center items-center p-16">
       <Image src={brand.src} alt="brandLogo" width={288} height={288} />
       <div className="flex flex-col mt-16 gap-8">
+        <Link href={GOOGLE_OAUTH_URL}>
+          <LoginButton method="google" />
+        </Link>
         <Link href={NAVER_OAUTH_URL}>
           <LoginButton method="naver" />
+        </Link>
+        <Link href={KAKAO_OAUTH_URL}>
+          <LoginButton method="kakao" />
         </Link>
       </div>
     </div>
