e29fba409255fc39deacca7c03bf3d9ab91cc7d2
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-30 18:22:29 +0900

    feat: 매치 결과 보는 UI 추가 (#175)
    * feat: 매치 결과 보는 UI 추가
    
    * chore: fix biome error

--- components/pages/LiveMatchList.tsx
+++ components/pages/LiveMatchList.tsx
@@ -2,9 +2,17 @@
 
 import DateCarousel from "@/components/DayCarousel";
 import { Button } from "@/components/ui/Button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Badge } from "@/components/ui/badge";
 import { getTierWithEmoji } from "@/utils/getTierWithEmoji";
 import { format } from "date-fns";
+import Link from "next/link";
+import SImage from "../ui/Image";
 
 const mockData = [
   {
@@ -189,16 +197,142 @@ const mockData = [
   },
 ];
 
-const getStatusText = (status: string) => {
-  switch (status) {
-    case "RECRUITING":
-      return "모집중";
-    case "COMPLETED":
-      return "모집 완료";
-    case "PLAYING":
-      return "경기중";
-    default:
-      return "알 수 없음";
+const matchMockData = [
+  {
+    id: 1,
+    stage: "8강 3세트",
+    team1: {
+      name: "유저1",
+      profileImage: "https://avatar.iran.liara.run/public/28",
+      score: 3,
+    },
+    team2: {
+      name: "유저2",
+      profileImage: "https://avatar.iran.liara.run/public/98",
+      score: 9,
+    },
+  },
+  {
+    id: 2,
+    stage: "4강 1세트",
+    team1: {
+      name: "유저3",
+      profileImage: "https://avatar.iran.liara.run/public/29",
+      score: 5,
+    },
+    team2: {
+      name: "유저4",
+      profileImage: "https://avatar.iran.liara.run/public/99",
+      score: 7,
+    },
+  },
+  {
+    id: 3,
+    stage: "결승",
+    team1: {
+      name: "유저5",
+      profileImage: "https://avatar.iran.liara.run/public/30",
+      score: 6,
+    },
+    team2: {
+      name: "유저6",
+      profileImage: "https://avatar.iran.liara.run/public/100",
+      score: 4,
+    },
+  },
+  {
+    id: 4,
+    stage: "4강 1세트",
+    team1: {
+      name: "유저3",
+      profileImage: "https://avatar.iran.liara.run/public/29",
+      score: 5,
+    },
+    team2: {
+      name: "유저4",
+      profileImage: "https://avatar.iran.liara.run/public/99",
+      score: 7,
+    },
+  },
+  {
+    id: 5,
+    stage: "결승",
+    team1: {
+      name: "유저5",
+      profileImage: "https://avatar.iran.liara.run/public/30",
+      score: 6,
+    },
+    team2: {
+      name: "유저6",
+      profileImage: "https://avatar.iran.liara.run/public/100",
+      score: 4,
+    },
+  },
+];
+
+type LeagueStatus = "RECRUITING" | "COMPLETED" | "PLAYING" | string;
+type LeagueTier = "GOLD" | "SILVER" | "BRONZE" | string;
+
+const renderLeagueStatusButton = (status: LeagueStatus) => {
+  if (status === "PLAYING") {
+    return (
+      <AccordionTrigger className="p-2 h-8 rounded-md text-xs w-[105px] border-0 bg-orange-500 text-white hover:no-underline">
+        진행 상황 보기
+      </AccordionTrigger>
+    );
+  }
+  if (status === "RECRUITING") {
+    return (
+      // TODO: 링크 지정하기
+      <Link href="/">
+        <Button className="p-2 h-8 rounded-md text-xs w-[105px] border-0 bg-blue-500 text-white">
+          모집중
+        </Button>
+      </Link>
+    );
+  }
+
+  if (status === "COMPLETED") {
+    return (
+      <Button className="p-2 h-8 rounded-md text-xs w-[105px] border-0 bg-gray-300 text-gray-600 hover:bg-gray-300 hover:text-gray-600 cursor-default">
+        모집마감
+      </Button>
+    );
+  }
+};
+
+const renderLeagueTierBadge = (tier: LeagueTier) => {
+  if (tier === "GOLD") {
+    return (
+      <Badge
+        variant="outline"
+        className="font-semibold px-2 py-0.5 text-xs rounded-full border-0 w-fit bg-yellow-200 text-yellow-800"
+      >
+        {getTierWithEmoji(tier)}
+      </Badge>
+    );
+  }
+
+  if (tier === "SILVER") {
+    return (
+      <Badge
+        variant="outline"
+        className="font-semibold px-2 py-0.5 text-xs rounded-full border-0 w-fit bg-gray-200 text-gray-700"
+      >
+        {getTierWithEmoji(tier)}
+      </Badge>
+    );
+  }
+
+  if (tier === "BRONZE") {
+    return (
+      <Badge
+        variant="outline"
+        className="font-semibold px-2 py-0.5 text-xs rounded-full border-0 w-fit bg-orange-200 text-orange-800"
+      >
+        {getTierWithEmoji(tier)}
+      </Badge>
+    );
   }
 };
 
@@ -208,74 +342,100 @@ function LiveMatchList() {
   };
 
   return (
-    <div className="p-4 w-full">
+    <div className="p-4 w-full bg-white">
       <DateCarousel onDateSelect={handleDateSelect} />
-      <div className="mt-4 space-y-4">
-        {mockData.map((item) => (
-          <div
-            key={item.league_id}
-            className="flex justify-between items-center p-4 border-b border-gray-200 !m-0"
-          >
-            <div className="flex flex-col items-center text-center mr-4">
-              <p className="font-bold text-gray-800">
-                {format(new Date(item.league_at), "d일")}
-              </p>
-              <p className="text-gray-500 ">
-                {format(new Date(item.league_at), "HH:mm")}
-              </p>
-            </div>
+      <div className="mt-3">
+        <Accordion type="single" collapsible>
+          {mockData.map((item) => (
+            <AccordionItem key={item.league_id} value={String(item.league_id)}>
+              <div className="flex justify-between items-center p-3 border-b border-gray-200 relative">
+                <div className="flex flex-col items-center text-center mr-3">
+                  <p className="font-bold text-gray-800 text-md">
+                    {format(new Date(item.league_at), "HH:mm")}
+                  </p>
+                </div>
+                <div className="flex flex-col flex-grow pl-3">
+                  <div className="pl-2">
+                    {renderLeagueTierBadge(item.required_tier)}
+                    <div className="flex items-center gap-1 mb-1">
+                      <p className="text-base font-semibold text-gray-900">
+                        {item.league_name}
+                      </p>
+                    </div>
 
-            <div className="flex flex-col flex-grow pl-4">
-              <div className="flex items-center gap-2 mb-1">
-                <Badge
-                  variant="outline"
-                  className="font-semibold px-2 py-1 border-primary"
-                >
-                  {getTierWithEmoji(item.required_tier) || "랭크 없음"}
-                </Badge>
-                <p className="text-lg font-semibold text-gray-900">
-                  {item.league_name}
-                </p>
+                    <div className="flex items-center gap-2 text-xs text-gray-500">
+                      <span>
+                        {item.match_type === "SINGLES" ? "단식" : "복식"}
+                      </span>
+                      <span className="text-gray-400">•</span>
+                      <span>
+                        {item.recruited_member_count} /{" "}
+                        {item.player_limit_count}명
+                      </span>
+
+                      {item.is_participated_in_league && (
+                        <>
+                          <span className="text-gray-400">•</span>
+                          <span className="text-blue-500 font-medium">
+                            참여 중
+                          </span>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-between items-center">
+                  {renderLeagueStatusButton(item.league_status)}
+                </div>
               </div>
-
-              <p className="text-gray-600 text-sm mb-1">
-                {item.league_location}
-              </p>
-              <div className="flex items-center gap-1 text-sm text-gray-500">
-                <span>{item.match_type === "SINGLES" ? "단식" : "복식"}</span>
-                <span className="text-gray-400 font-extralight">•</span>
-                <span>
-                  {item.recruited_member_count} / {item.player_limit_count}명
-                </span>
-
-                {item.is_participated_in_league && (
-                  <>
-                    <span className="text-gray-400 font-extralight">•</span>
-                    <span className="text-[#007bff] font-medium">참여 중</span>
-                  </>
-                )}
-              </div>
-            </div>
-
-            <Button
-              variant="outline"
-              className={`px-4 py-2 rounded-md font-semibold ${
-                item.league_status === "RECRUITING"
-                  ? "bg-primary text-white hover:bg-blue-500" // 모집 중: 부드러운 파란색
-                  : item.league_status === "COMPLETED"
-                    ? "bg-gray-200 text-gray-800 hover:bg-gray-200 hover:text-gray-800" // 모집 완료: 중립적인 회색
-                    : item.league_status === "PLAYING"
-                      ? "bg-red-500 text-white hover:bg-red-400 " // 경기 중: 부드러운 골드 계열
-                      : ""
-              }`}
-            >
-              {getStatusText(item.league_status)}
-            </Button>
-          </div>
-        ))}
+              <AccordionContent>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+                  {matchMockData.map((match) => (
+                    <div
+                      key={match.id}
+                      className="p-4 rounded-lg w-full flex flex-col justify-center items-center border border-solid border-gray-300 "
+                    >
+                      <Badge className="bg-yellow-500 hover:bg-yellow-500 text-xs font-semibold text-center mb-2 rounded-sm">
+                        {match.stage}
+                      </Badge>
+                      <div className="flex items-center justify-between gap-6">
+                        <div className="flex items-center space-x-3">
+                          <SImage
+                            src={match.team1.profileImage}
+                            radius="circular"
+                            width={45}
+                            height={45}
+                            alt="profile"
+                          />
+                          <span className="text-gray-800 text-sm font-semibold">
+                            {match.team1.name}
+                          </span>
+                        </div>
+                        <span className="text-gray-900 font-bold text-lg">
+                          {match.team1.score} : {match.team2.score}
+                        </span>
+                        <div className="flex items-center space-x-3">
+                          <span className="text-gray-800 text-sm font-semibold">
+                            {match.team2.name}
+                          </span>
+                          <SImage
+                            src={match.team2.profileImage}
+                            radius="circular"
+                            width={45}
+                            height={45}
+                            alt="profile"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
     </div>
   );
 }
-
 export default LiveMatchList;
