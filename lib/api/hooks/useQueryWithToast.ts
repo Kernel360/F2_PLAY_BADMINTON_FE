70310df3871d3f2c689959e644a0a7d527f13121
Yejin0O0 <yyj11kr@naver.com>
2024-10-31 17:11:40 +0900

    fix: useQueryWithToast 에러 해결

--- lib/api/hooks/useQueryWithToast.ts
+++ lib/api/hooks/useQueryWithToast.ts
@@ -2,16 +2,25 @@ import { useToast } from "@/hooks/use-toast";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-type CommonResponse<T> =
-  | {
-      result: "SUCCESS";
-      data: T;
-    }
-  | {
-      result: "FAIL";
-      message: string;
-      error_code: string;
-    };
+// type CommonResponse<T> =
+//   | {
+//       result: "SUCCESS";
+//       data: T;
+//     }
+//   | {
+//       result: "FAIL";
+//       message: string;
+//       error_code: string;
+//     };
+
+interface CommonResponse<T> {
+  result?: "SUCCESS" | "FAIL";
+  data?: T;
+  message?: string;
+  error_code?: string;
+  error_message_for_log?: string;
+  error_message_for_client?: string;
+}
 
 const useQueryWithToast = <TData>(
   queryKey: string[],
@@ -23,7 +32,7 @@ const useQueryWithToast = <TData>(
   useEffect(() => {
     if (queryResult.error) {
       toast({
-        title: "오류가 발생했습니다.",
+        title: queryResult.error.message,
         variant: "destructive",
       });
     }
