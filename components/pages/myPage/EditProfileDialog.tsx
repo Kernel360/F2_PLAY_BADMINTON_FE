4ff085660451e0575e3b0f0b6f422487f2dd3333
Yejin0O0 <yyj11kr@naver.com>
2024-11-07 16:32:36 +0900

    feat: 프로필 변경 유효성 검사 추가

--- components/pages/myPage/EditProfileDialog.tsx
+++ components/pages/myPage/EditProfileDialog.tsx
@@ -8,29 +8,62 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { Label } from "@/components/ui/label";
 import {
   usePostMembersProfileImage,
   usePutMembersProfile,
 } from "@/lib/api/hooks/memberHook";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Camera, Edit } from "lucide-react";
-import { type ChangeEvent, useEffect, useRef, useState } from "react";
+import { type ChangeEvent, useState } from "react";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const profileSchema = z.object({
+  name: z
+    .string()
+    .min(2, "닉네임은 2자 이상 10자 이내로 입력하세요.")
+    .max(10, "닉네임은 2자 이상 10자 이내로 입력하세요."),
+  profileImage: z
+    .instanceof(File)
+    .optional()
+    .refine(
+      (file) =>
+        file && ["image/jpeg", "image/png", "image/gif"].includes(file.type),
+      "이미지는 jpg, jpeg, png, gif 형식만 가능합니다.",
+    ),
+});
+
+type ProfileFormInputs = z.infer<typeof profileSchema>;
 
 interface EditProfileDialogProps {
   initialName: string;
   initialProfileImage: string;
 }
 
-export default function EditProfileDialog({
+function EditProfileDialog({
   initialName,
   initialProfileImage,
 }: EditProfileDialogProps) {
   const { mutate: postProfileImage } = usePostMembersProfileImage();
   const { mutate: putProfile } = usePutMembersProfile();
-  const nameRef = useRef<HTMLInputElement | null>(null);
   const [profileImage, setProfileImage] = useState<string>(initialProfileImage);
   const [uploadedImageFile, setUploadedImageFile] = useState<File | null>(null);
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const form = useForm<ProfileFormInputs>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      name: initialName,
+    },
+  });
 
   const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -38,32 +71,34 @@ export default function EditProfileDialog({
       const imageUrl = URL.createObjectURL(file);
       setProfileImage(imageUrl);
       setUploadedImageFile(file);
+      form.setValue("profileImage", file);
     }
   };
 
   const handleImageRemove = () => {
     setProfileImage("/images/dummy-image.jpg");
     setUploadedImageFile(null);
+    form.setValue("profileImage", undefined);
   };
 
-  const handleSave = () => {
-    const name = nameRef.current?.value || ""; // nameRef에서 값 추출
-    console.log(name);
+  const onSubmit: SubmitHandler<ProfileFormInputs> = (data) => {
+    const { name } = data;
     if (uploadedImageFile) {
       const formData = new FormData();
       formData.append("multipartFile", uploadedImageFile);
-
       postProfileImage(formData, {
         onSuccess: (response) => {
           if (response.data) {
-            console.log(response.data);
             putProfile(
               {
                 name,
                 profile_image_url: response.data,
               },
               {
-                onSuccess: () => setOpen(false),
+                onSuccess: () => {
+                  setOpen(false);
+                  setUploadedImageFile(null);
+                },
               },
             );
           }
@@ -104,57 +139,80 @@ export default function EditProfileDialog({
           닉네임과 프로필 이미지를 수정할 수 있습니다.
         </DialogDescription>
 
-        <div className="flex flex-col items-center gap-4 mt-6">
-          {/* 프로필 이미지 */}
-          <div className="relative">
-            <Avatar className="w-24 h-24 rounded-full">
-              <AvatarImage src={profileImage} alt="프로필 미리보기" />
-              <AvatarFallback />
-            </Avatar>
-            <div className="absolute bottom-0 right-0 bg-black bg-opacity-60 rounded-full p-1 cursor-pointer">
-              <label htmlFor="file-input" className="cursor-pointer">
-                <Camera className="text-white h-5 w-5" />
-              </label>
-              <input
-                id="file-input"
-                type="file"
-                className="hidden"
-                onChange={handleImageUpload}
-              />
-            </div>
-          </div>
-
-          {profileImage !== "/images/dummy-image.jpg" && (
-            <button
-              type="button"
-              onClick={handleImageRemove}
-              className="text-sm text-gray-500 mt-2"
-            >
-              이미지 삭제
-            </button>
-          )}
-
-          <div className="w-full mt-4 flex flex-col gap-2">
-            <Label htmlFor="name" className="text-gray-700">
-              닉네임
-            </Label>
-            <input
-              id="name"
-              type="text"
-              defaultValue={initialName}
-              ref={nameRef}
-              placeholder="닉네임을 입력하세요"
-              className="mt-1 h-10 rounded-sm text-black border border-gray-300"
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col items-center gap-4 mt-6"
+          >
+            <FormField
+              name="profileImage"
+              control={form.control}
+              render={() => (
+                <FormItem className="w-full flex flex-col gap-2 justify-center items-center">
+                  <div className="relative">
+                    <Avatar className="w-24 h-24 rounded-full">
+                      <AvatarImage src={profileImage} alt="프로필 미리보기" />
+                      <AvatarFallback />
+                    </Avatar>
+                    <div className="absolute bottom-0 right-0 bg-black bg-opacity-60 rounded-full p-1 cursor-pointer">
+                      <label htmlFor="file-input" className="cursor-pointer">
+                        <Camera className="text-white h-5 w-5" />
+                      </label>
+                      <input
+                        id="file-input"
+                        type="file"
+                        className="hidden"
+                        accept="image/jpeg, image/png, image/gif"
+                        onChange={handleImageUpload}
+                      />
+                    </div>
+                  </div>
+                  {profileImage !== "/images/dummy-image.jpg" && (
+                    <button
+                      type="button"
+                      onClick={handleImageRemove}
+                      className="text-sm text-gray-500 mt-2"
+                    >
+                      이미지 삭제
+                    </button>
+                  )}
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-        </div>
 
-        <DialogFooter>
-          <Button variant="outline" onClick={handleSave} className="w-full">
-            저장
-          </Button>
-        </DialogFooter>
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="w-full flex flex-col gap-2 justify-center items-start">
+                  <Label htmlFor="name" className="text-gray-700">
+                    닉네임
+                  </Label>
+                  <FormControl className="w-full">
+                    <input
+                      id="name"
+                      {...field}
+                      placeholder="닉네임을 입력하세요"
+                      // value={initialName}
+                      className="mt-1 h-10 rounded-sm text-black border border-gray-300"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter className="mt-4 w-full">
+              <Button type="submit" variant="default" className="w-full">
+                저장
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
 }
+
+export default EditProfileDialog;
