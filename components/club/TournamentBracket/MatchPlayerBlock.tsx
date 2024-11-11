556385bd6228820661baee56047b77f048c8b295
Yejin0O0 <yyj11kr@naver.com>
2024-11-11 17:10:52 +0900

    feat: update MatchPlayerBlock design

--- components/club/TournamentBracket/MatchPlayerBlock.tsx
+++ components/club/TournamentBracket/MatchPlayerBlock.tsx
@@ -1,4 +1,4 @@
-import { getTierWithEmoji } from "@/utils/getTierWithEmoji";
+import { getTierWithEmoji } from "@/utils/getTier";
 import React from "react";
 
 export type Player = {
@@ -8,17 +8,32 @@ export type Player = {
   score: number;
 };
 
-interface PlayerNodeProps extends Player {}
+interface MatchPlayerBlockProps extends Player {}
 
-function PlayerNode({ imgUrl, name, tier, score }: PlayerNodeProps) {
+function MatchPlayerBlock({
+  imgUrl,
+  name,
+  tier,
+  score,
+}: MatchPlayerBlockProps) {
   return (
-    <div className="flex gap-2 border-gray-300 border">
-      <img src={imgUrl} alt="profile" className="w-10 h-10 rounded-full" />
-      <span>{name}</span>
-      <span>{getTierWithEmoji(tier)}</span>
-      <span>{score}</span>
+    <div className="flex items-center gap-3 p-3 rounded-md border border-gray-300 shadow-sm bg-white w-56 hover:shadow-md transition-shadow">
+      <img
+        src={imgUrl}
+        alt="profile"
+        className="w-8 h-8 rounded-full object-cover border border-gray-200"
+      />
+      <div className="flex-1 flex items-center gap-1">
+        <span className="block text-xs text-gray-500">
+          {getTierWithEmoji(tier)}
+        </span>
+        <span className="block text-sm font-medium text-gray-800 truncate">
+          {name}
+        </span>
+      </div>
+      <span className="text-lg font-bold text-gray-900">{score}</span>
     </div>
   );
 }
 
-export default PlayerNode;
+export default MatchPlayerBlock;
