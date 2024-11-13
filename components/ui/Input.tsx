92b980ac56790703c3901e500d8e1b0e467d2e90
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-11-13 20:40:19 +0900

    feat: 경기 수정 input disable 처리 및 유효성 검사 구현 (#205)
    * feat: select 적용
    
    * fix: select default value 설정하기
    
    * fix: 경기 수정 input disable 설정
    
    * feat: 유효성 검사 적용
    
    * fix: fix biome error
    
    * fix: fix tsc error

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,92 +1,19 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { type VariantProps, cva } from "class-variance-authority";
 
-const inputVariants = cva("", {
-  variants: {
-    size: {
-      sm: "h-8 w-full px-2",
-      md: "h-10 w-full px-2",
-      lg: "h-12 w-full px-2",
-    },
-    radius: {
-      sm: "rounded-sm",
-      md: "rounded-md",
-      lg: "rounded-lg",
-      round: "rounded-full",
-    },
-  },
-  defaultVariants: {
-    size: "md",
-    radius: "md",
-  },
-});
-
-export interface InputProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">,
-    VariantProps<typeof inputVariants> {
-  icon?: React.ReactNode;
-  search?: boolean;
-}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  (
-    {
-      className,
-      size,
-      radius,
-      type,
-      placeholder = "placeholder",
-      icon,
-      search,
-      ...props
-    },
-    ref,
-  ) => {
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
     return (
-      <div
+      <input
+        type={type}
         className={cn(
-          "flex flex-row border border-solid items-center",
-          inputVariants({ size, radius }),
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-black ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className,
         )}
-      >
-        {icon && (
-          <span className={cn("inset-y-0 left-0 flex items-center")}>
-            {icon}
-          </span>
-        )}
-        {search && (
-          <span className={cn("inset-y-0 left-0 flex items-center")}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="gray"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-search"
-            >
-              <title>Search Icon</title>
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.3-4.3" />
-            </svg>
-          </span>
-        )}
-        <input
-          placeholder={placeholder}
-          type={type}
-          className={cn(
-            "outline-none bg-background text-black",
-            inputVariants({ size }),
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
+        ref={ref}
+        {...props}
+      />
     );
   },
 );
