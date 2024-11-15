9389db48715890712f076e451746ceed73de73b4
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-11-15 11:42:24 +0900

    design: 싱글 토너먼트 구현 (#208)
    * feat: update MatchPlayerBlock design
    
    * feat: add getTier
    
    * feat: 구조 구현
    
    * feat: 디렉터님과 페어프로그래밍
    
    * fix: 가운데 정렬
    
    * feat: 주석 삭제 및 로직 수정
    
    * feat: match 페이지에 적용
    
    * fix: fix tsc error

--- components/pages/club/Match.tsx
+++ components/pages/club/Match.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import MatchProfileDoubles from "@/components/club/MatchProfileDoubles";
+import TournamentBracket from "@/components/club/TournamentBracket";
 // import MatchProfileSingles from "@/components/club/MatchProfileSingles";
 // import { useGetMatches } from "@/lib/api/hooks/matchHook";
 import type { components } from "@/schemas/schema";
@@ -9,96 +10,52 @@ import { useMemo, useState } from "react";
 
 // type MatchResponse = components["schemas"]["MatchResponse"];
 
-// function Match() {
-//   const params = useParams();
-//   const [matchDialog, setMatchDialog] = useState<number | null>(null);
+function Match() {
+  //   const params = useParams();
+  //   const [matchDialog, setMatchDialog] = useState<number | null>(null);
 
-//   /* TODO: useParams를 이용하여 URL id 가져오기, id 타입 넣어주기 기본은 string*/
-//   const { data, isLoading, error } = useGetMatches(
-//     Number(params.clubId),
-//     Number(params.leagueId),
-//   );
+  //   /* TODO: useParams를 이용하여 URL id 가져오기, id 타입 넣어주기 기본은 string*/
+  //   const { data, isLoading, error } = useGetMatches(
+  //     Number(params.clubId),
+  //     Number(params.leagueId),
+  //   );
 
-//   const [singleGames, doubleGames] = useMemo<
-//     [MatchResponse[], MatchResponse[]]
-//   >(() => {
-//     if (data) {
-//       const singleGames: MatchResponse[] = [];
-//       const doubleGames: MatchResponse[] = [];
+  //   const [singleGames, doubleGames] = useMemo<
+  //     [MatchResponse[], MatchResponse[]]
+  //   >(() => {
+  //     if (data) {
+  //       const singleGames: MatchResponse[] = [];
+  //       const doubleGames: MatchResponse[] = [];
 
-//       for (const match of data) {
-//         if (match.match_type === "SINGLES") {
-//           singleGames.push(match);
-//         } else if (match.match_type === "DOUBLES") {
-//           doubleGames.push(match);
-//         }
-//       }
+  //       for (const match of data) {
+  //         if (match.match_type === "SINGLES") {
+  //           singleGames.push(match);
+  //         } else if (match.match_type === "DOUBLES") {
+  //           doubleGames.push(match);
+  //         }
+  //       }
 
-//       return [singleGames, doubleGames];
-//     }
+  //       return [singleGames, doubleGames];
+  //     }
 
-//     return [[], []];
-//   }, [data]);
+  //     return [[], []];
+  //   }, [data]);
 
-//   const toggleMatchDialog = (index: number) => {
-//     setMatchDialog((prevIndex) => (prevIndex === index ? null : index));
-//   };
+  //   const toggleMatchDialog = (index: number) => {
+  //     setMatchDialog((prevIndex) => (prevIndex === index ? null : index));
+  //   };
 
-//   if (isLoading) {
-//     <div>Loading...</div>;
-//   }
+  //   if (isLoading) {
+  //     <div>Loading...</div>;
+  //   }
 
-//   if (error) {
-//     return (
-//       <div className="text-red-500">데이터를 불러오는 데 실패했습니다.</div>
-//     );
-//   }
+  //   if (error) {
+  //     return (
+  //       <div className="text-red-500">데이터를 불러오는 데 실패했습니다.</div>
+  //     );
+  //   }
 
-//   return (
-//     <div>
-//       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-//         {singleGames.map((match) => {
-//           if (!match.singles_match) {
-//             return null;
-//           }
-//           return (
-//             <button
-//               type="button"
-//               key={match.match_id}
-//               onClick={() => toggleMatchDialog(match.match_id as number)}
-//             >
-//               <MatchProfileSingles
-//                 singlesMatch={match.singles_match}
-//                 isOpen={matchDialog === match.match_id}
-//                 onClose={() => toggleMatchDialog(match.match_id as number)}
-//               />
-//             </button>
-//           );
-//         })}
-//       </div>
+  return <TournamentBracket />;
+}
 
-//       <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-10 w-full flex-wrap mt-5">
-//         {doubleGames.map((match) => {
-//           if (!match.doubles_match) {
-//             return null;
-//           }
-//           return (
-//             <button
-//               type="button"
-//               key={match.match_id}
-//               onClick={() => toggleMatchDialog(match.match_id as number)}
-//             >
-//               <MatchProfileDoubles
-//                 doublesMatch={match.doubles_match}
-//                 isOpen={matchDialog === match.match_id}
-//                 onClose={() => toggleMatchDialog(match.match_id as number)}
-//               />
-//             </button>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Match;
+export default Match;
---
+++ types/matchTypes.ts
@@ -0,0 +1,5 @@
+import type { components } from "@/schemas/schema";
+
+export type MatchParticipant = components["schemas"]["Participant"];
+
+export type MatchBracketData = components["schemas"]["BracketResponse"];
