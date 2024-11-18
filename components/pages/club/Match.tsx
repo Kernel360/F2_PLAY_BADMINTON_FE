4d2ededcbfa53eae72693130a492f5aaa4823b02
Yejin0O0 <yyj11kr@naver.com>
2024-11-18 10:54:18 +0900

    feat: add Loading

--- components/pages/club/Match.tsx
+++ components/pages/club/Match.tsx
@@ -1,22 +1,24 @@
 "use client";
 
 import FreeBracket from "@/components/club/FreeBracket";
-import MatchProfileDoubles from "@/components/club/MatchProfileDoubles";
 import TournamentBracket from "@/components/club/TournamentBracket";
-// import MatchProfileSingles from "@/components/club/MatchProfileSingles";
 import { useGetMatches } from "@/lib/api/hooks/matchHook";
-import type { components } from "@/schemas/schema";
 import type { GetMatchesData } from "@/types/matchTypes";
 import { useParams } from "next/navigation";
-import { useMemo, useState } from "react";
-
-// type MatchResponse = components["schemas"]["MatchResponse"];
 
 function Match() {
   const { clubId, leagueId } = useParams();
 
   // data의 match_generation_type에 따라 다른 컴포넌트 렌더링해줘야 함
-  const { data } = useGetMatches(clubId as string, Number(leagueId));
+  const { data, isLoading } = useGetMatches(clubId as string, Number(leagueId));
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-[464px] w-full">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <>
