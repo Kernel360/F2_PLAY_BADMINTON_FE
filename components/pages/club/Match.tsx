cb7c2b9a4c54f17de463a18bc3ad2cba2061bdae
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-11-18 10:57:39 +0900

    design: 프리 형식 대진표 ui 생성 (#214)
    * feat: FreeBracket 완성
    
    * feat: add Loading
    
    * feat: add Loading

--- components/pages/club/Match.tsx
+++ components/pages/club/Match.tsx
@@ -1,23 +1,34 @@
 "use client";
 
-import MatchProfileDoubles from "@/components/club/MatchProfileDoubles";
+import FreeBracket from "@/components/club/FreeBracket";
 import TournamentBracket from "@/components/club/TournamentBracket";
-// import MatchProfileSingles from "@/components/club/MatchProfileSingles";
 import { useGetMatches } from "@/lib/api/hooks/matchHook";
-import type { components } from "@/schemas/schema";
 import type { GetMatchesData } from "@/types/matchTypes";
 import { useParams } from "next/navigation";
-import { useMemo, useState } from "react";
-
-// type MatchResponse = components["schemas"]["MatchResponse"];
 
 function Match() {
   const { clubId, leagueId } = useParams();
 
   // data의 match_generation_type에 따라 다른 컴포넌트 렌더링해줘야 함
-  const { data } = useGetMatches(clubId as string, Number(leagueId));
+  const { data, isLoading } = useGetMatches(clubId as string, Number(leagueId));
 
-  return <TournamentBracket nodeData={data as GetMatchesData} />;
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-[464px] w-full">
+        Loading...
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {data?.match_generation_type === "FREE" ? (
+        <FreeBracket nodeData={data as GetMatchesData} />
+      ) : (
+        <TournamentBracket nodeData={data as GetMatchesData} />
+      )}
+    </>
+  );
 }
 
 export default Match;
