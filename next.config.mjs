5ec5a23c89a37bb4eadaf57ee68d3bb8697d1c65
Yejin0O0 <yyj11kr@naver.com>
2024-11-19 15:04:13 +0900

    feat: 카카오 프로필 사진 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       "badminton-team.s3.ap-northeast-2.amazonaws.com",
       "ssl.pstatic.net", // 네이버 프로필 사진
       "lh3.googleusercontent.com", // 구글 프로필 사진
+      "k.kakaocdn.net", // 카카오 프로필 사진
       "d36om9pjoifd2y.cloudfront.net",
       "avatar.iran.liara.run",
     ],
