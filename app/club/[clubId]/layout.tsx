93cfc7ac8d9b334f86c49e30e17e4c6b9a69f16f
Yejin0O0 <yyj11kr@naver.com>
2024-11-26 19:04:03 +0900

    design: 동호회 레이아웃 변경

--- app/club/[clubId]/layout.tsx
+++ app/club/[clubId]/layout.tsx
@@ -1,61 +1,8 @@
-"use client";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/Tabs";
-import { useGetClubMembersCheck } from "@/lib/api/hooks/clubMemberHook";
-// import { useGetIsClubMember } from "@/lib/api/hooks/memberHook";
-import { headers } from "next/headers";
-import Link from "next/link";
-import { useParams, usePathname } from "next/navigation";
-import type React from "react";
+import ClubLayout from "@/components/layouts/ClubLayout";
+import type { ReactNode } from "react";
 
-function ClubLayout({ children }: { children: React.ReactNode }) {
-  const { clubId } = useParams();
-
-  const pathname = usePathname();
-  const clubPath = pathname.split("/")[3];
-
-  const { data: isJoined } = useGetClubMembersCheck(clubId as string);
-
-  const activeTab = () => {
-    if (Array.isArray(clubPath) || clubPath === undefined) return "intro";
-    return clubPath;
-  };
-
-  return (
-    <Tabs defaultValue={activeTab()} className="w-full max-w-5xl mt-10 ">
-      <TabsList>
-        <Link href={`/club/${clubId}`}>
-          <TabsTrigger value="intro" color="gray">
-            소개
-          </TabsTrigger>
-        </Link>
-
-        <>
-          <Link href={`/club/${clubId}/league`}>
-            <TabsTrigger value="league" color="gray">
-              일정
-            </TabsTrigger>
-          </Link>
-          {isJoined?.data?.is_club_member && (
-            <Link href={`/club/${clubId}/member`}>
-              <TabsTrigger value="member" color="gray">
-                회원
-              </TabsTrigger>
-            </Link>
-          )}
-          {isJoined?.data?.role === "ROLE_OWNER" && (
-            <Link href={`/club/${clubId}/manage`}>
-              <TabsTrigger value="manage" color="gray">
-                관리
-              </TabsTrigger>
-            </Link>
-          )}
-        </>
-      </TabsList>
-      <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow flex-1 space-y-4 p-8 min-h-[530px]">
-        {children}
-      </div>
-    </Tabs>
-  );
+function layout({ children }: { children: React.ReactNode }) {
+  return <ClubLayout>{children}</ClubLayout>;
 }
 
-export default ClubLayout;
+export default layout;
--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { type VariantProps, cva } from "class-variance-authority";
 
 const Tabs = TabsPrimitive.Root;
 
@@ -13,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 mb-4 items-center justify-center rounded-full bg-white p-1 text-gray-500",
+      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
       className,
     )}
     {...props}
@@ -21,37 +22,15 @@ const TabsList = React.forwardRef<
 ));
 TabsList.displayName = TabsPrimitive.List.displayName;
 
-const tabTriggerVariants = cva("", {
-  variants: {
-    color: {
-      gray: "data-[state=active]:bg-muted data-[state=active]:text-black",
-      purple:
-        "data-[state=active]:bg-purple-200 data-[state=active]:text-purple-500",
-      blue: "data-[state=active]:bg-blue-200 data-[state=active]:text-blue-500",
-      red: "data-[state=active]:bg-red-200 data-[state=active]:text-red-500",
-    },
-  },
-  defaultVariants: {
-    color: "gray",
-  },
-});
-
-export interface TabTriggerProps
-  extends VariantProps<typeof tabTriggerVariants> {
-  color?: "gray" | "purple" | "blue" | "red";
-}
-
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> & TabTriggerProps
->(({ className, color = "gray", ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      // 'inline-flex items-center justify-center whitespace-nowrap rounded-full px-4 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-md',
-      "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-black font-medium text-gray-500",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
       className,
-      tabTriggerVariants({ color }),
     )}
     {...props}
   />
@@ -62,8 +41,15 @@ const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
 >(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content ref={ref} className={cn(className)} {...props} />
+  <TabsPrimitive.Content
+    ref={ref}
+    className={cn(
+      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      className,
+    )}
+    {...props}
+  />
 ));
 TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export { Tabs, TabsList, TabsTrigger, TabsContent, tabTriggerVariants };
+export { Tabs, TabsList, TabsTrigger, TabsContent };
