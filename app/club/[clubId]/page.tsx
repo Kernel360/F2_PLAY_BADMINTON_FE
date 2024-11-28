46c505d668c76e37e7a1f956bc5d24cf573e3e6b
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-11-28 17:16:40 +0900

    Revert "feat: 클럽 컴포넌트에서 prefetchQuery 적용함 (#270)" (#271)
    This reverts commit bddbf575ce20ecb06a4e851ecb3ecea42a78d62d.

--- app/club/[clubId]/page.tsx
+++ app/club/[clubId]/page.tsx
@@ -1,46 +1,7 @@
 import ClubIntro from "@/components/pages/club/ClubIntro";
-import { getClubsById } from "@/lib/api/functions/clubFn";
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from "@tanstack/react-query";
-import type { Metadata } from "next";
 
-type Props = {
-  params: Promise<{ clubId: string }>;
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const clubId = (await params).clubId;
-
-  const club = await getClubsById(clubId);
-
-  if (!club.data) return {};
-
-  return {
-    title: club.data.club_name,
-    description: club.data.club_description,
-    openGraph: {
-      images: [club.data.club_image],
-    },
-  };
-}
-
-async function ClubIntroPage({ params }: Props) {
-  const clubId = (await params).clubId;
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["clubsDataById"],
-    queryFn: () => getClubsById(clubId),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ClubIntro />
-    </HydrationBoundary>
-  );
+function ClubIntroPage() {
+  return <ClubIntro />;
 }
 
 export default ClubIntroPage;
