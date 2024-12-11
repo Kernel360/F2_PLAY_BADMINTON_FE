bafebfd9fa55e6b16145e22ef648c30fb4eaa149
Yejin0O0 <yyj11kr@naver.com>
2024-12-11 21:15:20 +0900

    feat: 스케줄 취소 처리

--- components/club/LeagueList/LeagueSchedule.tsx
+++ components/club/LeagueList/LeagueSchedule.tsx
@@ -5,6 +5,7 @@ import type { GetLeagueDateData } from "@/types/leagueTypes";
 import { getLeagueType } from "@/utils/getLeagueType";
 import { getTierWithEmojiAndText } from "@/utils/getTier";
 import { format } from "date-fns";
+import { TriangleAlert } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect } from "react";
 
@@ -27,45 +28,67 @@ function LeagueSchedule(props: LeagueScheduleProps) {
   }, [selectedDate, schedulesRefetch]);
 
   if (schedules !== undefined && schedules.length > 0) {
-    return schedules.map((schedule: GetLeagueDateData) => (
-      <Link
-        key={schedule.league_id}
-        href={`/club/${clubId}/league/${schedule.league_id}`}
-      >
-        <div className="bg-white py-4 px-6 rounded-xl border border-solid hover:shadow-lg transform transition-transform duration-300 cursor-pointer">
-          <div className="flex justify-between items-start mb-4">
-            <div>
-              <h2 className="text-lg font-bold text-gray-900">
-                {schedule.league_name}
-              </h2>
-              <Text color="gray" className="text-sm mt-1">
-                {getLeagueType(schedule.match_type as string)}
-              </Text>
+    return schedules.map((schedule: GetLeagueDateData) => {
+      const isCanceled = schedule.status === "CANCELED";
+
+      return (
+        <Link
+          key={schedule.league_id}
+          href={
+            isCanceled ? "#" : `/club/${clubId}/league/${schedule.league_id}`
+          }
+          className={isCanceled ? "pointer-events-none" : ""}
+        >
+          <div className="relative bg-white py-4 px-6 rounded-xl border border-solid hover:shadow-lg transform transition-transform duration-300 cursor-pointer">
+            {isCanceled && (
+              <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center gap-2 rounded-xl z-10 text-white">
+                <TriangleAlert size={15} />
+                <span className="font-semibold text-sm">
+                  이 경기는 취소되었습니다
+                </span>
+                <TriangleAlert size={15} />
+              </div>
+            )}
+
+            {/* Content */}
+            <div className={`${isCanceled ? "opacity-50" : ""}`}>
+              <div className="flex justify-between items-start mb-4">
+                <div>
+                  <h2 className="text-lg font-bold text-gray-900">
+                    {schedule.league_name}
+                  </h2>
+                  <Text color="gray" className="text-sm mt-1">
+                    {getLeagueType(schedule.match_type as string)}
+                  </Text>
+                </div>
+                <Text className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
+                  {getTierWithEmojiAndText(schedule.required_tier as string)}
+                </Text>
+              </div>
+              <div className="flex justify-between items-center text-gray-600">
+                <div>
+                  <Text className="text-sm">
+                    모집 기한:{" "}
+                    {format(
+                      new Date(schedule.recruiting_close_at as string),
+                      "MM/dd",
+                    )}
+                  </Text>
+                </div>
+                <div>
+                  <Text className="text-sm">
+                    {schedule.participant_count} / {schedule.player_limit_count}{" "}
+                    명
+                  </Text>
+                </div>
+              </div>
             </div>
-            <Text className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
-              {getTierWithEmojiAndText(schedule.required_tier as string)}
-            </Text>
           </div>
-          <div className="flex justify-between items-center text-gray-600">
-            <div>
-              <Text className="text-sm">
-                모집 기한:{" "}
-                {format(
-                  new Date(schedule.recruiting_close_at as string),
-                  "MM/dd",
-                )}
-              </Text>
-            </div>
-            <div>
-              <Text className="text-sm">
-                {schedule.participant_count} / {schedule.player_limit_count} 명
-              </Text>
-            </div>
-          </div>
-        </div>
-      </Link>
-    ));
+        </Link>
+      );
+    });
   }
+
   if (isLoading) {
     return (
       <div className="space-y-4">
